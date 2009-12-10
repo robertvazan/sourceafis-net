758628b6820b1fefc84dd4e129b2b96b85db2897
robertvanak <robertvanak@localhost>
2009-12-10 00:09:44 +0000

    Added empty Fingerprint Analyzer window

---
+++ FingerprintAnalyzer/Program.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace FingerprintAnalyzer
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainWindow());
+        }
+    }
+}
