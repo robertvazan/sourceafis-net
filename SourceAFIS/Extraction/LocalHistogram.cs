86514d3e72e0af4bdbcf4e5f03f78092de0fb024
Robert Vazan <robvcode@gmail.com>
2009-12-16 10:43:48 +0000

    Compute everything on block areas rather than in corner areas

--- SourceAFIS/Extraction/LocalHistogram.cs
+++ SourceAFIS/Extraction/LocalHistogram.cs
@@ -10,15 +10,34 @@ namespace SourceAFIS.Extraction
     {
         public short[, ,] Analyze(BlockMap blocks, byte[,] image)
         {
-            short[, ,] histogram = new short[blocks.CornerCount.Height, blocks.CornerCount.Width, 256];
-            Threader.Split<Point>(blocks.CornerList, delegate(Point corner)
+            short[, ,] histogram = new short[blocks.BlockCount.Height, blocks.BlockCount.Width, 256];
+            Threader.Split<Point>(blocks.BlockList, delegate(Point block)
             {
-                RectangleC area = blocks.CornerAreas[corner.Y, corner.X];
+                RectangleC area = blocks.BlockAreas[block.Y, block.X];
                 for (int y = area.Bottom; y < area.Top; ++y)
                     for (int x = area.Left; x < area.Right; ++x)
-                        ++histogram[corner.Y, corner.X, image[y, x]];
+                        ++histogram[block.Y, block.X, image[y, x]];
             });
             return histogram;
+        }
+
+        public short[, ,] SmoothAroundCorners(BlockMap blocks, short[, ,] input)
+        {
+            Point[] blocksAround = new Point[] { new Point(0, 0), new Point(-1, 0), new Point(0, -1), new Point(-1, -1) };
+            short[, ,] output = new short[blocks.CornerCount.Height, blocks.CornerCount.Width, 256];
+            Threader.Split<Point>(blocks.CornerList, delegate(Point corner)
+            {
+                foreach (Point relative in blocksAround)
+                {
+                    Point block = Calc.Add(corner, relative);
+                    if (blocks.BlockRect.Contains(block))
+                    {
+                        for (int i = 0; i < 256; ++i)
+                            output[corner.Y, corner.X, i] += input[block.Y, block.X, i];
+                    }
+                }
+            });
+            return output;
         }
 
         public short[, ,] Smooth(BlockMap blocks, short[, ,] input)
