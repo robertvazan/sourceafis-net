2b0b54c398bdc0d7447f1ee861adc45f948f5c50
Robert Vazan <robvcode@gmail.com>
2010-01-14 23:36:36 +0000

    Added template serialization

---
+++ SourceAFIS/Extraction/Templates/SerializedFormat.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace SourceAFIS.Extraction.Templates
+{
+    public sealed class SerializedFormat : TemplateFormatBase<Template>
+    {
+        public override Template Export(TemplateBuilder builder)
+        {
+            return new Template(builder);
+        }
+
+        public override TemplateBuilder Import(Template template)
+        {
+            return template.ToTemplateBuilder();
+        }
+
+        public override void Serialize(Stream stream, Template template)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(stream, template);
+        }
+
+        public override Template Deserialize(Stream stream)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            return (Template)formatter.Deserialize(stream);
+        }
+    }
+}
