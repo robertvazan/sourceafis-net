59d47c89e7f9588aab5d6d6c9733143a13930e50
Robert Vazan <robvcode@gmail.com>
2010-01-19 16:25:24 +0000

    Mask type in options window

---
+++ FingerprintAnalyzer/MaskType.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FingerprintAnalyzer
+{
+    enum MaskType
+    {
+        None,
+        Segmentation,
+        Inner
+    }
+}
