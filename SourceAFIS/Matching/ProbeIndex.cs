f3d3a07d50d0b02b32c8766228d8380c116697a4
Robert Vazan <robvcode@gmail.com>
2010-02-27 17:22:32 +0000

    Read-only ProbeIndex that can be shared by multiple threads

---
+++ SourceAFIS/Matching/ProbeIndex.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using SourceAFIS.Extraction.Templates;
+
+namespace SourceAFIS.Matching
+{
+    public sealed class ProbeIndex
+    {
+        public Template Template;
+        public ProbeNeighbors Neighbors;
+    }
+}
