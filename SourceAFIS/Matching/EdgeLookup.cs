84f6df4429de49789ad8b2e350e43e8a651fd2d2
robertvanak <robertvanak@localhost>
2010-02-28 16:14:51 +0000

    Faster edge lookup

---
+++ SourceAFIS/Matching/EdgeLookup.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using SourceAFIS.Meta;
+using SourceAFIS.General;
+
+namespace SourceAFIS.Matching
+{
+    public sealed class EdgeLookup
+    {
+        [DpiAdjusted]
+        [Parameter(Lower = 0, Upper = 50)]
+        public int MaxDistanceError = 20;
+        [Parameter]
+        public byte MaxAngleError = Angle.FromDegreesB(10);
+
+        public struct EdgePair
+        {
+            public int ProbeIndex;
+            public int CandidateIndex;
+
+            public EdgePair(int probe, int candidate)
+            {
+                ProbeIndex = probe;
+                CandidateIndex = candidate;
+            }
+        }
+
+        List<EdgePair> ReturnList = new List<EdgePair>();
+
+        public List<EdgePair> FindMatchingPairs(NeighborEdge[] probeStar, NeighborEdge[] candidateStar)
+        {
+            ReturnList.Clear();
+            Range range = new Range();
+            for (int candidateIndex = 0; candidateIndex < candidateStar.Length; ++candidateIndex)
+            {
+                NeighborEdge candidateEdge = candidateStar[candidateIndex];
+                while (range.Begin < probeStar.Length && probeStar[range.Begin].Edge.Length < candidateEdge.Edge.Length - MaxDistanceError)
+                    ++range.Begin;
+                if (range.End < range.Begin)
+                    range.End = range.Begin;
+                while (range.End < probeStar.Length && probeStar[range.End].Edge.Length <= candidateEdge.Edge.Length + MaxDistanceError)
+                    ++range.End;
+                for (int probeIndex = range.Begin; probeIndex < range.End; ++probeIndex)
+                {
+                    if (Angle.Distance(probeStar[probeIndex].Edge.ReferenceAngle, candidateEdge.Edge.ReferenceAngle) <= MaxAngleError
+                        && Angle.Distance(probeStar[probeIndex].Edge.NeighborAngle, candidateEdge.Edge.NeighborAngle) <= MaxAngleError)
+                    {
+                        ReturnList.Add(new EdgePair(probeIndex, candidateIndex));
+                    }
+                }
+            }
+            return ReturnList;
+        }
+    }
+}
