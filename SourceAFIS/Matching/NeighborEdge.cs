0f223587eed3807ad3129a6ca5c96257c0c123a5
Robert Vazan <robvcode@gmail.com>
2010-02-28 15:23:26 +0000

    Construct edge lookup table for candidate template in order to improve performance

---
+++ SourceAFIS/Matching/NeighborEdge.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SourceAFIS.Matching
+{
+    public struct NeighborEdge
+    {
+        public EdgeInfo Edge;
+        public int Neighbor;
+    }
+}
