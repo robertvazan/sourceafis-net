b297ba2785a1f9c8080fb4ecdece4cdfbd0e562f
robertvanak <robertvanak@localhost>
2010-02-28 18:04:34 +0000

    Compute matcher EER

--- DatabaseAnalyzer/MatcherReport.cs
+++ DatabaseAnalyzer/MatcherReport.cs
@@ -16,16 +16,23 @@ namespace DatabaseAnalyzer
             CreateDocument("matcher-report");
             XmlElement prepare = AddChild("prepare");
             AddProperty(prepare, "count", Benchmark.Prepares.Count);
-            AddProperty(prepare, "microseconds", Benchmark.Prepares.Milliseconds * 1000);
+            AddProperty(prepare, "milliseconds", Benchmark.Prepares.Milliseconds, 3);
             CreateMatches(Benchmark.Matches, "matches");
             CreateMatches(Benchmark.NonMatches, "non-matches");
+            XmlElement table = AddChild("database-table");
+            for (int i = 0; i < Benchmark.TestDatabase.Databases.Count; ++i)
+            {
+                XmlElement database = AddChild(table, "database");
+                AddProperty(database, "eer", Benchmark.PerDatabaseEER[i] * 100, 3);
+            }
+            AddProperty("eer", Benchmark.EER * 100, 3);
         }
 
         void CreateMatches(MatcherBenchmark.Statistics statistics, string name)
         {
             XmlElement matches = AddChild(name);
             AddProperty(matches, "count", statistics.Count);
-            AddProperty(matches, "microseconds", statistics.Milliseconds * 1000);
+            AddProperty(matches, "milliseconds", statistics.Milliseconds, 3);
         }
     }
 }
--- SourceAFIS/Tuning/MatcherBenchmark.cs
+++ SourceAFIS/Tuning/MatcherBenchmark.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using SourceAFIS.General;
 using SourceAFIS.Extraction.Templates;
 using SourceAFIS.Matching;
 
@@ -16,6 +17,7 @@ namespace SourceAFIS.Tuning
             public BenchmarkTimer Timer = new BenchmarkTimer();
             public int Count;
             public double Milliseconds;
+            public float[][][][] ScoreTable;
         }
 
         public sealed class PrepareStats
@@ -29,10 +31,16 @@ namespace SourceAFIS.Tuning
         public Statistics Matches = new Statistics();
         public Statistics NonMatches = new Statistics();
 
+        public double[] PerDatabaseEER;
+        public double EER;
+
         public void Run()
         {
-            foreach (TestDatabase.Database database in TestDatabase.Databases)
+            Matches.ScoreTable = InitScoreTable();
+            NonMatches.ScoreTable = InitScoreTable();
+            for (int databaseIndex = 0; databaseIndex < TestDatabase.Databases.Count; ++databaseIndex)
             {
+                TestDatabase.Database database = TestDatabase.Databases[databaseIndex];
                 for (int fingerIndex = 0; fingerIndex < database.Fingers.Count; ++fingerIndex)
                 {
                     TestDatabase.Finger finger = database.Fingers[fingerIndex];
@@ -45,17 +53,29 @@ namespace SourceAFIS.Tuning
                         for (int candidateView = 0; candidateView < finger.Views.Count; ++candidateView)
                             if (candidateView != viewIndex)
                                 matching.Add(finger.Views[candidateView].Template);
-                        RunMatch(matching, Matches);
+                        Matches.ScoreTable[databaseIndex][fingerIndex][viewIndex] = RunMatch(matching, Matches);
 
                         List<Template> nonmatching = new List<Template>();
                         for (int candidateFinger = 0; candidateFinger < database.Fingers.Count; ++candidateFinger)
                             if (candidateFinger != fingerIndex)
                                 nonmatching.Add(database.Fingers[candidateFinger].Views[viewIndex].Template);
-                        RunMatch(nonmatching, NonMatches);
+                        NonMatches.ScoreTable[databaseIndex][fingerIndex][viewIndex] = RunMatch(nonmatching, NonMatches);
                     }
                 }
             }
             Summarize();
+        }
+
+        float[][][][] InitScoreTable()
+        {
+            float[][][][] result = new float[TestDatabase.Databases.Count][][][];
+            for (int database = 0; database < result.Length; ++database)
+            {
+                result[database] = new float[TestDatabase.Databases[database].Fingers.Count][][];
+                for (int finger = 0; finger < result[database].Length; ++finger)
+                    result[database][finger] = new float[TestDatabase.Databases[database].Fingers[finger].Views.Count][];
+            }
+            return result;
         }
 
         void RunPrepare(Template template)
@@ -66,12 +86,13 @@ namespace SourceAFIS.Tuning
             ++Prepares.Count;
         }
 
-        void RunMatch(List<Template> templates, Statistics statistics)
+        float[] RunMatch(List<Template> templates, Statistics statistics)
         {
             statistics.Timer.Start();
-            Matcher.Match(templates);
+            float[] scores = Matcher.Match(templates);
             statistics.Timer.Stop();
             statistics.Count += templates.Count;
+            return scores;
         }
 
         void Summarize()
@@ -79,6 +100,85 @@ namespace SourceAFIS.Tuning
             Prepares.Milliseconds = Prepares.Timer.Accumulated.TotalMilliseconds / Prepares.Count;
             Matches.Milliseconds = Matches.Timer.Accumulated.TotalMilliseconds / Matches.Count;
             NonMatches.Milliseconds = NonMatches.Timer.Accumulated.TotalMilliseconds / NonMatches.Count;
+            SummarizeScores();
+        }
+
+        struct ScoreRecord
+        {
+            public float Score;
+            public int Matches;
+            public int NonMatches;
+
+            public ScoreRecord(float score, bool match)
+            {
+                Score = score;
+                Matches = match ? 1 : 0;
+                NonMatches = match ? 0 : 1;
+            }
+        }
+
+        void SummarizeScores()
+        {
+            PerDatabaseEER = new double[TestDatabase.Databases.Count];
+            for (int database = 0; database < TestDatabase.Databases.Count; ++database)
+            {
+                List<ScoreRecord> mixed = new List<ScoreRecord>();
+                int totalMatching = 0;
+                foreach (float[][] finger in Matches.ScoreTable[database])
+                    foreach (float[] view in finger)
+                    {
+                        foreach (float score in view)
+                            mixed.Add(new ScoreRecord(score, true));
+                        totalMatching += view.Length;
+                    }
+                int totalNonMatching = 0;
+                foreach (float[][] finger in NonMatches.ScoreTable[database])
+                    foreach (float[] view in finger)
+                    {
+                        foreach (float score in view)
+                            mixed.Add(new ScoreRecord(score, false));
+                        totalNonMatching += view.Length;
+                    }
+                mixed.Sort(delegate(ScoreRecord left, ScoreRecord right) { return Calc.Compare(left.Score, right.Score); });
+
+                List<ScoreRecord> compact = new List<ScoreRecord>();
+                ScoreRecord last = mixed[0];
+                for (int i = 1; i < mixed.Count; ++i)
+                {
+                    if (mixed[i].Score > last.Score)
+                    {
+                        compact.Add(last);
+                        last = mixed[i];
+                    }
+                    else
+                    {
+                        last.Matches += mixed[i].Matches;
+                        last.NonMatches += mixed[i].NonMatches;
+                    }
+                }
+                compact.Add(last);
+
+                PerDatabaseEER[database] = 0.5;
+                int matchesSoFar = 0;
+                int nonmatchesSoFar = 0;
+                foreach (ScoreRecord record in compact)
+                {
+                    matchesSoFar += record.Matches;
+                    nonmatchesSoFar += record.NonMatches;
+                    int falseMatches = totalNonMatching - nonmatchesSoFar;
+                    int falseNonMatches = matchesSoFar;
+                    double FAR = falseMatches / (double)totalNonMatching;
+                    double FRR = falseNonMatches / (double)totalMatching;
+                    double PerThresholdEER = (FAR + FRR) / 2;
+                    if (PerThresholdEER < PerDatabaseEER[database])
+                        PerDatabaseEER[database] = PerThresholdEER;
+                }
+            }
+
+            EER = 0;
+            foreach (double eer in PerDatabaseEER)
+                EER += eer;
+            EER /= PerDatabaseEER.Length;
         }
     }
 }
