ada35f4596a928eacbc775c6e187cf97dcca3f92
Robert Vazan <robvcode@gmail.com>
2010-02-28 16:28:33 +0000

    Use complementary angle comparison in lookup for performance reasons

--- SourceAFIS/Matching/EdgeLookup.cs
+++ SourceAFIS/Matching/EdgeLookup.cs
@@ -30,26 +30,34 @@ namespace SourceAFIS.Matching
 
         public List<EdgePair> FindMatchingPairs(NeighborEdge[] probeStar, NeighborEdge[] candidateStar)
         {
+            byte complementaryAngleError = Angle.Complementary(MaxAngleError);
             ReturnList.Clear();
             Range range = new Range();
+
             for (int candidateIndex = 0; candidateIndex < candidateStar.Length; ++candidateIndex)
             {
                 NeighborEdge candidateEdge = candidateStar[candidateIndex];
+                
                 while (range.Begin < probeStar.Length && probeStar[range.Begin].Edge.Length < candidateEdge.Edge.Length - MaxDistanceError)
                     ++range.Begin;
                 if (range.End < range.Begin)
                     range.End = range.Begin;
                 while (range.End < probeStar.Length && probeStar[range.End].Edge.Length <= candidateEdge.Edge.Length + MaxDistanceError)
                     ++range.End;
+
                 for (int probeIndex = range.Begin; probeIndex < range.End; ++probeIndex)
                 {
-                    if (Angle.Distance(probeStar[probeIndex].Edge.ReferenceAngle, candidateEdge.Edge.ReferenceAngle) <= MaxAngleError
-                        && Angle.Distance(probeStar[probeIndex].Edge.NeighborAngle, candidateEdge.Edge.NeighborAngle) <= MaxAngleError)
+                    byte referenceDiff = Angle.Difference(probeStar[probeIndex].Edge.ReferenceAngle, candidateEdge.Edge.ReferenceAngle);
+                    byte neighborDiff = Angle.Difference(probeStar[probeIndex].Edge.NeighborAngle, candidateEdge.Edge.NeighborAngle);
+                    
+                    if ((referenceDiff <= MaxAngleError || referenceDiff >= complementaryAngleError)
+                        && (neighborDiff <= MaxAngleError || neighborDiff >= complementaryAngleError))
                     {
                         ReturnList.Add(new EdgePair(probeIndex, candidateIndex));
                     }
                 }
             }
+
             return ReturnList;
         }
     }
