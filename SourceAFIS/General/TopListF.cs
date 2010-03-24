b709605df4fae585d8c768eb94f58dca9d991596
robertvanak <robertvanak@localhost>
2010-03-24 00:22:59 +0000

    Report fingerprint pairs with most severe errors in computed score

---
+++ SourceAFIS/General/TopListF.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SourceAFIS.General
+{
+    public sealed class TopListF<V>
+        where V : new()
+    {
+        struct Item
+        {
+            public float Key;
+            public V Value;
+
+            public Item(float key, V value)
+            {
+                Key = key;
+                Value = value;
+            }
+        }
+
+        readonly int Capacity;
+        List<Item> List = new List<Item>();
+
+        public TopListF(int capacity)
+        {
+            Capacity = capacity;
+        }
+
+        public void Add(float key, V value)
+        {
+            List.Add(new Item(key, value));
+            if (List.Count > 2 * Capacity)
+                Sort();
+        }
+
+        void Sort()
+        {
+            List.Sort(delegate(Item left, Item right) { return Calc.Compare(left.Key, right.Key); });
+            if (List.Count > Capacity)
+                List.RemoveRange(Capacity, List.Count - Capacity);
+        }
+
+        public V[] GetValues()
+        {
+            Sort();
+            return List.ConvertAll<V>(delegate(Item item) { return item.Value; }).ToArray();
+        }
+    }
+}
---
+++ SourceAFIS/Tuning/Reports/TopErrors.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+using SourceAFIS.General;
+using SourceAFIS.Tuning.Errors;
+
+namespace SourceAFIS.Tuning.Reports
+{
+    public sealed class TopErrors
+    {
+        public const int TopErrorCount = 5;
+
+        public struct Pair
+        {
+            public ScoreTable.Index Probe;
+            public ScoreTable.Index Candidate;
+            [XmlAttribute]
+            public float Score;
+        }
+        
+        public struct PerDatabase
+        {
+            public Pair[] TopFalseAccepts;
+            public Pair[] TopFalseRejects;
+        }
+
+        public PerDatabase[] Databases;
+
+        public void Compute(ScoreTable[] tables)
+        {
+            Databases = new PerDatabase[tables.Length];
+            for (int i = 0; i < tables.Length; ++i)
+            {
+                Databases[i].TopFalseAccepts = CollectFalseAccepts(tables[i]);
+                Databases[i].TopFalseRejects = CollectFalseRejects(tables[i]);
+            }
+        }
+
+        public static Pair[] CollectFalseAccepts(ScoreTable table)
+        {
+            TopListF<Pair> top = new TopListF<Pair>(TopErrorCount);
+            for (int finger = 0; finger < table.Table.Length; ++finger)
+                for (int view = 0; view < table.Table[finger].Length; ++view)
+                    for (int pair = 0; pair < table.Table[finger][view].NonMatching.Length; ++pair)
+                    {
+                        Pair item = new Pair();
+                        item.Probe.Finger = finger;
+                        item.Probe.View = view;
+                        item.Candidate = item.Probe;
+                        for (int i = 0; i <= pair; ++i)
+                        {
+                            do
+                            {
+                                item.Candidate.Finger = (item.Candidate.Finger + 1) % table.Table.Length;
+                            } while (view >= table.Table[item.Candidate.Finger].Length);
+                        }
+                        item.Score = table.Table[finger][view].NonMatching[pair];
+                        top.Add(-table.Table[finger][view].NonMatching[pair], item);
+                    }
+            return top.GetValues();
+        }
+
+        public static Pair[] CollectFalseRejects(ScoreTable table)
+        {
+            TopListF<Pair> top = new TopListF<Pair>(TopErrorCount);
+            for (int finger = 0; finger < table.Table.Length; ++finger)
+                for (int view = 0; view < table.Table[finger].Length; ++view)
+                    for (int pair = 0; pair < table.Table[finger][view].Matching.Length; ++pair)
+                    {
+                        Pair item = new Pair();
+                        item.Probe.Finger = finger;
+                        item.Probe.View = view;
+                        item.Candidate.Finger = finger;
+                        item.Candidate.View = (view + pair + 1) % table.Table[finger].Length;
+                        item.Score = table.Table[finger][view].Matching[pair];
+                        top.Add(table.Table[finger][view].Matching[pair], item);
+                    }
+            return top.GetValues();
+        }
+    }
+}
