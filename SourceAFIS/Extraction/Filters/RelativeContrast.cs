5362c7048392508135a163e621c78b048e689103
Robert Vazan <robvcode@gmail.com>
2010-03-25 22:23:17 +0000

    Saved parameters found by optimizer

--- SourceAFIS/Extraction/Filters/RelativeContrast.cs
+++ SourceAFIS/Extraction/Filters/RelativeContrast.cs
@@ -11,7 +11,7 @@ namespace SourceAFIS.Extraction.Filters
     {
         [DpiAdjusted]
         [Parameter(Lower = 10 * 10, Upper = 2000 * 2000)]
-        public int SampleSize = 73952;
+        public int SampleSize = 73813;
         [Parameter]
         public float SampleFraction = 0.49f;
         [Parameter]
