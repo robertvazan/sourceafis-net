da7b12d36906d436a8af549bc88ffda408eed7ce
robertvanak <robertvanak@localhost>
2010-03-25 13:33:33 +0000

    Saved parameters found by optimizer

--- SourceAFIS/Extraction/Model/PoreRemover.cs
+++ SourceAFIS/Extraction/Model/PoreRemover.cs
@@ -11,7 +11,7 @@ namespace SourceAFIS.Extraction.Model
     {
         [DpiAdjusted]
         [Parameter(Lower = 3, Upper = 100)]
-        public int MaxArmLength = 48;
+        public int MaxArmLength = 45;
 
         [Nested]
         public KnotRemover KnotRemover = new KnotRemover();
