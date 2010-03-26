134b546f457fce040b0afe9f2703c9cbea942d2a
robertvanak <robertvanak@localhost>
2010-03-26 01:23:24 +0000

    Saved parameters found by optimizer

--- SourceAFIS/Matching/MatchScoring.cs
+++ SourceAFIS/Matching/MatchScoring.cs
@@ -8,15 +8,15 @@ namespace SourceAFIS.Matching
     public sealed class MatchScoring
     {
         [Parameter(Upper = 10)]
-        public float PairCountFactor = 0.66f;
+        public float PairCountFactor = 0.48f;
         [Parameter(Upper = 100)]
-        public float PairFractionFactor = 8.55f;
+        public float PairFractionFactor = 8.26f;
         [Parameter(Upper = 10)]
-        public float CorrectTypeFactor = 0.01f;
+        public float CorrectTypeFactor = 0.1f;
         [Parameter(Upper = 10)]
-        public float SupportedCountFactor = 0.24f;
+        public float SupportedCountFactor = 0.4f;
         [Parameter(Upper = 10, Precision = 3)]
-        public float EdgeCountFactor = 0.078f;
+        public float EdgeCountFactor = 0.208f;
 
         public float Compute(MatchAnalysis analysis)
         {
