7aeee2032ae054966dbcbe49b34ccd62784dc3be
Robert Vazan <robvcode@gmail.com>
2010-04-14 23:16:15 +0000

    Executables conforming to FVC-onGoing requirements

---
+++ FvcEnroll/Enroll.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Drawing;
+using SourceAFIS.Simple;
+
+namespace FvcEnroll
+{
+    class Enroll
+    {
+        static void WriteLog(string[] args, string status)
+        {
+            using (FileStream stream = File.Open(args[2], FileMode.Append))
+            {
+                using (TextWriter writer = new StreamWriter(stream))
+                    writer.WriteLine("{0} {1} {2}", args[0], args[1], status);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                AfisEngine afis = new AfisEngine();
+                Fingerprint fp = new Fingerprint();
+                using (Image image = Image.FromFile(args[0]))
+                {
+                    using (Bitmap bitmap = new Bitmap(image))
+                        fp.BitmapImage = bitmap;
+                }
+                afis.Extract(fp);
+                File.WriteAllBytes(args[1], fp.Template);
+                WriteLog(args, "OK");
+            }
+            catch (Exception)
+            {
+                WriteLog(args, "FAIL");
+                throw;
+            }
+        }
+    }
+}
---
+++ FvcEnroll/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FvcEnroll")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FvcEnroll")]
+[assembly: AssemblyCopyright("Copyright ©  2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3bb926e9-db98-4982-bc47-d4d2c59a2a6c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
