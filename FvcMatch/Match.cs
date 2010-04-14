8bcf46e556059bb1ac5bce9953753ff6ee13bdbc
robertvanak <robertvanak@localhost>
2010-04-14 23:16:15 +0000

    Executables conforming to FVC-onGoing requirements

---
+++ FvcMatch/Match.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using SourceAFIS.Simple;
+
+namespace FvcMatch
+{
+    class Match
+    {
+        static Person LoadTemplate(string path)
+        {
+            Fingerprint fp = new Fingerprint();
+            fp.Template = File.ReadAllBytes(path);
+            Person person = new Person();
+            person.Add(fp);
+            return person;
+        }
+
+        static void WriteLog(string[] args, string status, float similarity)
+        {
+            using (FileStream stream = File.Open(args[2], FileMode.Append))
+            {
+                using (TextWriter writer = new StreamWriter(stream))
+                    writer.WriteLine("{0} {1} {2} {3}", args[0], args[1], status, similarity);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                AfisEngine afis = new AfisEngine();
+                Person probe = LoadTemplate(args[0]);
+                Person candidate = LoadTemplate(args[1]);
+                afis.Threshold = 0;
+                float score = afis.Verify(probe, candidate);
+                float similarity = score / 100;
+                WriteLog(args, "OK", similarity);
+            }
+            catch (Exception)
+            {
+                WriteLog(args, "FAIL", 0);
+            }
+        }
+    }
+}
---
+++ FvcMatch/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FvcMatch")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FvcMatch")]
+[assembly: AssemblyCopyright("Copyright ©  2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("dce232a0-2c46-4d5b-9c86-ba5cb75676d4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
