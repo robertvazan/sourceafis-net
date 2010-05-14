d1141411f2523a9411766807a8953ef73b6fb581
robertvanak <robertvanak@localhost>
2010-05-14 19:07:36 +0000

    Empty build project

---
+++ AfisBuilder/AfisBuilder.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AfisBuilder
+{
+    class AfisBuilder
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
