c9c4629c2b17f55d3881206c943c8de8ca1a69e1
Robert Vazan <robvcode@gmail.com>
2010-05-17 21:54:54 +0000

    Assemble ZIP package

--- AfisBuilder/AfisBuilder.cs
+++ AfisBuilder/AfisBuilder.cs
@@ -8,6 +8,7 @@ namespace AfisBuilder
 {
     class AfisBuilder
     {
+        string OutputFolder;
         Regex VersionRegex;
         string BuildVersion;
 
@@ -19,7 +20,7 @@ namespace AfisBuilder
             {
                 Match match = VersionRegex.Match(line);
                 if (match.Success)
-                    BuildVersion = match.Groups[0].Value;
+                    BuildVersion = match.Groups[1].Value;
             }
             if (BuildVersion == null)
                 throw new ApplicationException("Release version not found.");
@@ -37,7 +38,7 @@ namespace AfisBuilder
                 if (match.Success)
                 {
                     found = true;
-                    Group group = match.Groups[0];
+                    Group group = match.Groups[1];
                     int from = group.Index;
                     int to = group.Index + group.Length;
                     lines[i] = lines[i].Substring(0, from) + BuildVersion + lines[i].Substring(to, lines[i].Length - to);
@@ -48,23 +49,81 @@ namespace AfisBuilder
             File.WriteAllLines(path, lines, Encoding.UTF8);
         }
 
-        public void Run()
+        void SetFolder()
         {
+            OutputFolder = Directory.GetCurrentDirectory();
             Directory.SetCurrentDirectory(@"..\..\..");
+        }
+
+        void UpdateVersions()
+        {
             CollectVersion();
             UpdateVersion("DatabaseAnalyzer");
             UpdateVersion("FingerprintAnalyzer");
             UpdateVersion("FvcEnroll");
             UpdateVersion("FvcMatch");
             UpdateVersion("Sample");
+        }
+
+        void BuildProjects()
+        {
             Command.Build(@"SourceAFIS\SourceAFIS.csproj", "Release");
             Command.Build(@"DatabaseAnalyzer\DatabaseAnalyzer.csproj", "Release");
             Command.Build(@"FingerprintAnalyzer\FingerprintAnalyzer.csproj", "Release");
             Command.Build(@"FvcEnroll\FvcEnroll.csproj", "Release");
             Command.Build(@"FvcMatch\FvcMatch.csproj", "Release");
-            File.Copy(@"SourceAFIS\bin\Release\SourceAFIS.dll", @"Sample\dll\SourceAFIS.dll", true);
+            Command.CopyTo(@"SourceAFIS\bin\Release\SourceAFIS.dll", @"Sample\dll");
+            if (Directory.Exists(@"Sample\bin"))
+                Directory.Delete(@"Sample\bin", true);
             Command.Build(@"Sample\Sample.csproj", "Debug");
             Command.Build(@"DocProject\DocProject.csproj", "Release");
+        }
+
+        void AssembleZip()
+        {
+            string path = OutputFolder + @"\SourceAFIS-" + BuildVersion;
+            Console.WriteLine("Assembling ZIP archive: {0}", path);
+            if (Directory.Exists(path))
+                Directory.Delete(path, true);
+            Directory.CreateDirectory(path);
+            path += @"\";
+
+            Directory.CreateDirectory(path + "Bin");
+            Command.CopyTo(@"SourceAFIS\bin\Release\SourceAFIS.dll", path + "Bin");
+            Command.CopyTo(@"SourceAFIS\bin\Release\SourceAFIS.xml", path + "Bin");
+            Command.CopyTo(@"DatabaseAnalyzer\bin\Release\DatabaseAnalyzer.exe", path + "Bin");
+            Command.CopyTo(@"Data\DatabaseAnalyzerConfiguration.xml", path + "Bin");
+            Command.CopyTo(@"FingerprintAnalyzer\bin\Release\FingerprintAnalyzer.exe", path + "Bin");
+
+            Directory.CreateDirectory(path + "Documentation");
+            Command.CopyTo(@"Data\license.txt", path + "Documentation");
+            Command.CopyTo(@"Data\SourceAFIS_Home.html", path + "Documentation");
+            Command.CopyTo(@"SourceAFIS\bin\Release\SourceAFIS.xml", path + "Documentation");
+            Command.CopyTo(@"DocProject\bin\Release\SourceAFIS.chm", path + "Documentation");
+            Command.CopyTo(@"DocProject\bin\Release\SourceAFIS.HxS", path + "Documentation");
+
+            Directory.CreateDirectory(path + @"Documentation\SourceAFIS");
+            Command.CopyDirectory(@"DocProject\Help\html", path + @"Documentation\SourceAFIS\html");
+            Command.CopyDirectory(@"DocProject\Help\Icons", path + @"Documentation\SourceAFIS\icons");
+            Command.CopyDirectory(@"DocProject\Help\Scripts", path + @"Documentation\SourceAFIS\scripts");
+            Directory.CreateDirectory(path + @"Documentation\SourceAFIS\styles");
+            File.Copy(@"DocProject\Help\Styles\Presentation.css", path + @"Documentation\SourceAFIS\styles\presentation.css");
+            Command.CopyTo(@"DocProject\Help\Styles\TopicDesigner.css", path + @"Documentation\SourceAFIS\styles");
+
+            Command.CopyDirectory("Sample", path + "Sample");
+            if (File.Exists(path + @"Sample\Sample.suo"))
+                File.Delete(path + @"Sample\Sample.suo");
+            Directory.Delete(path + @"Sample\obj", true);
+
+            Command.Zip(OutputFolder + @"\SourceAFIS-" + BuildVersion + ".zip", path);
+        }
+
+        public void Run()
+        {
+            SetFolder();
+            UpdateVersions();
+            BuildProjects();
+            AssembleZip();
         }
 
         static void Main(string[] args)
