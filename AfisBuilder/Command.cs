ae243563e7c9d87af15662b6f4faed2f9765cfd8
robertvanak <robertvanak@localhost>
2010-05-17 21:54:54 +0000

    Assemble ZIP package

--- AfisBuilder/Command.cs
+++ AfisBuilder/Command.cs
@@ -8,6 +8,21 @@ namespace AfisBuilder
 {
     class Command
     {
+        public static void CopyTo(string file, string folder)
+        {
+            File.Copy(file, folder + @"\" + Path.GetFileName(file), true);
+        }
+
+        public static void CopyDirectory(string from, string to)
+        {
+            Directory.CreateDirectory(to);
+            foreach (string filename in Directory.GetFiles(from))
+                CopyTo(filename, to);
+            foreach (string subfolder in Directory.GetDirectories(from))
+                if (Path.GetFileName(subfolder) != ".svn")
+                    CopyDirectory(subfolder, to + @"\" + Path.GetFileName(subfolder));
+        }
+
         public static void Build(string project, string configuration)
         {
             string[] versions = Directory.GetDirectories(@"C:\WINDOWS\Microsoft.NET\Framework", "v2.0.*");
@@ -15,6 +30,13 @@ namespace AfisBuilder
                 throw new ApplicationException("Cannot find msbuild tool.");
             string msbuildPath = versions[versions.Length - 1] + @"\msbuild.exe";
             Execute(msbuildPath, "/t:Rebuild", "/p:configuration=" + configuration, project);
+        }
+
+        public static void Zip(string archive, string folder)
+        {
+            Execute(@"C:\Program Files\7-Zip\7z.exe", "a", "-tzip", archive, folder);
+            if (!File.Exists(archive))
+                throw new ApplicationException("No ZIP file was created.");
         }
 
         public static void Execute(string command, params string[] parameters)
