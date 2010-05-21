7e2319b8067a566c71c943fe9c0156fc93a6aaac
robertvanak <robertvanak@localhost>
2010-05-21 11:11:46 +0000

    Add missing files to WiX project, also fixed folder IDs

--- AfisBuilder/WiX.cs
+++ AfisBuilder/WiX.cs
@@ -15,6 +15,7 @@ namespace AfisBuilder
 
         static string SourceFolder;
         static List<string> Folders;
+        static List<string> Files;
 
         public static void Load(string path)
         {
@@ -45,6 +46,16 @@ namespace AfisBuilder
             SourceFolder = path;
             Folders = new List<string>();
             ScanFolders(SourceFolder, "");
+        }
+
+        public static void ScanFiles()
+        {
+            Files = new List<string>();
+            foreach (string folder in Folders)
+            {
+                foreach (string path in Directory.GetFiles(SourceFolder + @"\" + folder))
+                    Files.Add(folder + @"\" + Path.GetFileName(path));
+            }
         }
 
         static XmlElement GetChildByName(XmlElement parent, string name)
@@ -85,6 +96,34 @@ namespace AfisBuilder
             return false;
         }
 
+        static XmlElement GetChildByNameAttr(XmlElement parent, string name)
+        {
+            foreach (XmlNode node in parent.ChildNodes)
+            {
+                if (node is XmlElement)
+                {
+                    XmlElement element = (XmlElement)node;
+                    if (element.GetAttribute("Name") == name)
+                        return element;
+                }
+            }
+            throw new ApplicationException("No element with such Id.");
+        }
+
+        static bool HasChildWithNameAttr(XmlElement parent, string name)
+        {
+            foreach (XmlNode node in parent.ChildNodes)
+            {
+                if (node is XmlElement)
+                {
+                    XmlElement element = (XmlElement)node;
+                    if (element.GetAttribute("Name") == name)
+                        return true;
+                }
+            }
+            return false;
+        }
+
         static XmlElement GetPFilesDirectory()
         {
             XmlElement targetdir = GetChildById(Product, "TARGETDIR");
@@ -99,7 +138,7 @@ namespace AfisBuilder
             else
             {
                 XmlElement parent = GetDirectoryElement(Path.GetDirectoryName(path));
-                return GetChildById(parent, Path.GetFileName(path));
+                return GetChildByNameAttr(parent, Path.GetFileName(path));
             }
         }
 
@@ -109,12 +148,101 @@ namespace AfisBuilder
             {
                 XmlElement parent = GetDirectoryElement(Path.GetDirectoryName(folder));
                 string name = Path.GetFileName(folder);
-                if (!HasChildWithId(parent, name))
+                string id = GetUniqueId(folder);
+                if (!HasChildWithNameAttr(parent, name))
                 {
                     XmlElement element = Document.CreateElement("Directory", "http://schemas.microsoft.com/wix/2006/wi");
-                    element.SetAttribute("Id", name);
+                    element.SetAttribute("Id", id);
                     element.SetAttribute("Name", name);
                     parent.AppendChild(element);
+                }
+            }
+        }
+
+        static string FilterFilename(string file)
+        {
+            const int MaxLength = 70;
+            const int EndLength = MaxLength / 2 - 1;
+            string filtered = file.Replace(@"\", "_").Replace("{", "_").Replace("}", "_").Replace("`", "_");
+            if (filtered.Length <= MaxLength)
+                return filtered;
+            else
+                return filtered.Substring(0, EndLength) + "__" + filtered.Substring(filtered.Length - EndLength, EndLength);
+        }
+
+        static bool ContainsIdAlready(XmlElement element, string id)
+        {
+            if (element.GetAttribute("Id") == id)
+                return true;
+            foreach (XmlNode node in element.ChildNodes)
+                if (node is XmlElement && ContainsIdAlready((XmlElement)node, id))
+                    return true;
+            return false;
+        }
+
+        static bool ContainsIdAlready(string id)
+        {
+            return ContainsIdAlready(Root, id);
+        }
+
+        static string GetUniqueId(string bare)
+        {
+            string filtered = FilterFilename(bare);
+            if (!ContainsIdAlready(filtered))
+                return filtered;
+            for (int i = 2; i < 100; ++i)
+            {
+                string numbered = filtered + i.ToString();
+                if (!ContainsIdAlready(numbered))
+                    return numbered;
+            }
+            throw new ApplicationException("Cannot generate unique ID.");
+        }
+
+        public static bool ContainsFile(XmlElement directory, string name)
+        {
+            foreach (XmlNode node in directory.ChildNodes)
+                if (node is XmlElement)
+                {
+                    XmlElement component = (XmlElement)node;
+                    if (component.Name == "Component")
+                    {
+                        XmlElement file = GetChildByName(component, "File");
+                        if (file.GetAttribute("Name") == name)
+                            return true;
+                    }
+                }
+            return false;
+        }
+
+        public static void AddMissingFiles()
+        {
+            XmlElement feature = GetChildByName(Product, "Feature");
+            foreach (string file in Files)
+            {
+                XmlElement directory = GetDirectoryElement(Path.GetDirectoryName(file));
+                string name = Path.GetFileName(file);
+                string fileid = GetUniqueId(file);
+                string componentid = GetUniqueId("component_" + file);
+                
+                if (!ContainsFile(directory, name))
+                {
+                    XmlElement component = Document.CreateElement("Component", "http://schemas.microsoft.com/wix/2006/wi");
+                    component.SetAttribute("Id", componentid);
+                    component.SetAttribute("Guid", Guid.NewGuid().ToString());
+                    directory.AppendChild(component);
+                    
+                    XmlElement fileref = Document.CreateElement("File", "http://schemas.microsoft.com/wix/2006/wi");
+                    fileref.SetAttribute("Id", fileid);
+                    fileref.SetAttribute("Name", name);
+                    fileref.SetAttribute("DiskId", "1");
+                    fileref.SetAttribute("Source", file);
+                    fileref.SetAttribute("KeyPath", "yes");
+                    component.AppendChild(fileref);
+
+                    XmlElement componentref = Document.CreateElement("ComponentRef", "http://schemas.microsoft.com/wix/2006/wi");
+                    componentref.SetAttribute("Id", componentid);
+                    feature.AppendChild(componentref);
                 }
             }
         }
