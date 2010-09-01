683f20807db4da2f17dd7ddc8ec2575e613a3ce8
robertvanak <robertvanak@localhost>
2010-09-01 21:48:44 +0200

    Fixed synchronization bug

--- SourceAFIS/Extraction/Filters/ThresholdBinarizer.cs
+++ SourceAFIS/Extraction/Filters/ThresholdBinarizer.cs
@@ -17,15 +17,18 @@ namespace SourceAFIS.Extraction.Filters
         public BinaryMap Binarize(float[,] input, float[,] baseline, BinaryMap mask, BlockMap blocks)
         {
             BinaryMap binarized = new BinaryMap(input.GetLength(1), input.GetLength(0));
-            Parallel.ForEach(blocks.AllBlocks, delegate(Point block)
+            Parallel.For(0, blocks.AllBlocks.Height, delegate(int blockY)
             {
-                if (mask.GetBit(block))
+                for (int blockX = 0; blockX < blocks.AllBlocks.Width; ++blockX)
                 {
-                    RectangleC rect = blocks.BlockAreas[block];
-                    for (int y = rect.Bottom; y < rect.Top; ++y)
-                        for (int x = rect.Left; x < rect.Right; ++x)
-                            if (input[y, x] - baseline[y, x] > 0)
-                                binarized.SetBitOne(x, y);
+                    if (mask.GetBit(blockX, blockY))
+                    {
+                        RectangleC rect = blocks.BlockAreas[blockY, blockX];
+                        for (int y = rect.Bottom; y < rect.Top; ++y)
+                            for (int x = rect.Left; x < rect.Right; ++x)
+                                if (input[y, x] - baseline[y, x] > 0)
+                                    binarized.SetBitOne(x, y);
+                    }
                 }
             });
             Logger.Log(binarized);
