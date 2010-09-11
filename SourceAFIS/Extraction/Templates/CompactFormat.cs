6c31e03224f48f7a56589c02af856e23d645ee01
Robert Vazan <robvcode@gmail.com>
2010-09-11 13:06:27 +0200

    CompactTemplate uses BinaryReader/Writer

--- SourceAFIS/Extraction/Templates/CompactFormat.cs
+++ SourceAFIS/Extraction/Templates/CompactFormat.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using System.Net;
 using SourceAFIS.Dummy;
+using SourceAFIS.General;
 
 namespace SourceAFIS.Extraction.Templates
 {
@@ -22,103 +24,88 @@ namespace SourceAFIS.Extraction.Templates
 
         static readonly byte[] Magic = new byte[] { 0x50, 0xBC, 0xAF, 0x15 }; // read "SorcAFIS"
 
-        byte[] FixByteOrder(byte[] bytes)
-        {
-            if (BitConverter.IsLittleEndian)
-                return bytes.Reverse().ToArray();
-            else
-                return bytes;
-        }
-
-        void EncodeShort(short value, byte[] template, int offset)
-        {
-            template[offset] = (byte)(((ushort)value) >> 8);
-            template[offset + 1] = (byte)(((ushort)value) & 0xFF);
-        }
-
-        void EncodeShort(int value, byte[] template, int offset)
-        {
-            if (value < Int16.MinValue || value > Int16.MaxValue)
-                throw new ArgumentOutOfRangeException();
-            EncodeShort((short)value, template, offset);
-        }
-
         public override byte[] Export(TemplateBuilder builder)
         {
-            byte[] template = new byte[4 + 1 + 2 + 2 + 6 * builder.Minutiae.Count];
-
-            // 4B magic
-            Magic.CopyTo(template, 0);
-
-            // 1B version
-            template[4] = 1;
-
-            // 2B total length, filled later
-            EncodeShort(template.Length, template, 5);
-
-            // 2B minutia count
-            EncodeShort(builder.Minutiae.Count, template, 7);
-
-            for (int i = 0; i < builder.Minutiae.Count; ++i)
+            checked
             {
-                // 2B position X
-                EncodeShort(builder.Minutiae[i].Position.X, template, 9 + 6 * i);
-                // 2B position Y
-                EncodeShort(builder.Minutiae[i].Position.Y, template, 9 + 6 * i + 2);
-                // 1B direction
-                template[9 + 6 * i + 4] = builder.Minutiae[i].Direction;
-                // 1B type
-                template[9 + 6 * i + 5] = (byte)builder.Minutiae[i].Type;
+                MemoryStream stream = new MemoryStream();
+                BinaryWriter writer = new BinaryWriter(stream, Encoding.UTF8);
+
+                // 4B magic
+                writer.Write(Magic);
+
+                // 1B version (current = 1)
+                writer.Write((byte)1);
+
+                // 2B total length (including magic), will be filled later
+                writer.Write((short)0);
+
+                // 2B minutia count
+                writer.Write(IPAddress.HostToNetworkOrder((short)builder.Minutiae.Count));
+
+                // N*6B minutia records
+                foreach (var minutia in builder.Minutiae)
+                {
+                    //      2B position X
+                    writer.Write(IPAddress.HostToNetworkOrder((short)minutia.Position.X));
+
+                    //      2B position Y
+                    writer.Write(IPAddress.HostToNetworkOrder((short)minutia.Position.Y));
+
+                    //      1B direction
+                    writer.Write(minutia.Direction);
+
+                    //      1B type
+                    writer.Write((byte)minutia.Type);
+                }
+
+                writer.Close();
+
+                // update length
+                byte[] template = stream.GetBuffer();
+                BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)template.Length)).CopyTo(template, 5);
+
+                return template;
             }
-
-            return template;
-        }
-
-        void DecodeConstraint(bool condition)
-        {
-            if (!condition)
-                throw new ApplicationException("Incorrect template format.");
-        }
-
-        short DecodeShort(byte[] template, int offset)
-        {
-            return (short)((template[offset] << 8) | template[offset + 1]);
         }
 
         public override TemplateBuilder Import(byte[] template)
         {
             TemplateBuilder builder = new TemplateBuilder();
 
+            MemoryStream stream = new MemoryStream(template);
+            BinaryReader reader = new BinaryReader(stream, Encoding.UTF8);
+
             // 4B magic
             for (int i = 0; i < Magic.Length; ++i)
-                DecodeConstraint(template[i] == Magic[i]);
+                AssertException.Check(reader.ReadByte() == Magic[i]);
 
             // 1B version (current = 1)
-            DecodeConstraint(template[4] == 1);
+            AssertException.Check(reader.ReadByte() == 1);
 
             // 2B total length (including magic)
-            DecodeConstraint(DecodeShort(template, 5) == template.Length);
+            reader.ReadInt16();
 
             // 2B minutia count
-            int minutiaCount = DecodeShort(template, 7);
-            DecodeConstraint(9 + 6 * minutiaCount == template.Length);
+            int minutiaCount = IPAddress.NetworkToHostOrder(reader.ReadInt16());
 
+            // N*6B minutia records
             for (int i = 0; i < minutiaCount; ++i)
             {
                 TemplateBuilder.Minutia minutia = new TemplateBuilder.Minutia();
 
-                // 2B position X
-                minutia.Position.X = DecodeShort(template, 9 + 6 * i);
+                //      2B position X
+                minutia.Position.X = IPAddress.NetworkToHostOrder(reader.ReadInt16());
 
-                // 2B position Y
-                minutia.Position.Y = DecodeShort(template, 9 + 6 * i + 2);
+                //      2B position Y
+                minutia.Position.Y = IPAddress.NetworkToHostOrder(reader.ReadInt16());
 
-                // 1B direction
-                minutia.Direction = template[9 + 6 * i + 4];
+                //      1B direction
+                minutia.Direction = reader.ReadByte();
 
-                // 1B type
-                minutia.Type = (TemplateBuilder.MinutiaType)template[9 + 6 * i + 5];
-                DecodeConstraint(Enum.IsDefined(typeof(TemplateBuilder.MinutiaType), minutia.Type));
+                //      1B type
+                minutia.Type = (TemplateBuilder.MinutiaType)reader.ReadByte();
+                AssertException.Check(Enum.IsDefined(typeof(TemplateBuilder.MinutiaType), minutia.Type));
 
                 builder.Minutiae.Add(minutia);
             }
@@ -134,21 +121,13 @@ namespace SourceAFIS.Extraction.Templates
         public override byte[] Deserialize(Stream stream)
         {
             byte[] header = new byte[7];
-            int headerBytes = stream.Read(header, 0, 7);
-            DecodeConstraint(headerBytes == 7);
+            stream.Read(header, 0, 7);
 
-            for (int i = 0; i < Magic.Length; ++i)
-                DecodeConstraint(header[i] == Magic[i]);
-            DecodeConstraint(header[4] == 1);
-
-            int length = DecodeShort(header, 5);
-            DecodeConstraint(length > header.Length);
+            int length = IPAddress.NetworkToHostOrder(BitConverter.ToInt16(header, 5));
 
             byte[] template = new byte[length];
             header.CopyTo(template, 0);
-            int templateBytes = stream.Read(template, 7, length - 7);
-            DecodeConstraint(7 + templateBytes == length);
-
+            stream.Read(template, 7, length - 7);
             return template;
         }
     }
