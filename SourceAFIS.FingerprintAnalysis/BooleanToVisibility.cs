b974d76404bd415a1f034b67237bfc1733e2554d
Robert Vazan <robvcode@gmail.com>
2010-09-19 19:49:03 +0200

    Controlling visibility by a checkbox

---
+++ SourceAFIS.FingerprintAnalysis/BooleanToVisibility.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Globalization;
+using System.Windows;
+
+namespace SourceAFIS.FingerprintAnalysis
+{
+    public class BooleanToVisibility : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                bool typed = bool.Parse(value.ToString());
+                return typed ? Visibility.Visible : Visibility.Collapsed;
+            }
+            catch (Exception)
+            {
+                return Visibility.Visible;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
