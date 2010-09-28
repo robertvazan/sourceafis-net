a40d76ac8387296110d9b30492745b9554217165
robertvanak <robertvanak@localhost>
2010-09-28 22:10:42 +0200

    Change notification for probe/candidate path

--- SourceAFIS.FingerprintAnalysis/Options.cs
+++ SourceAFIS.FingerprintAnalysis/Options.cs
@@ -1,13 +1,26 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel;
 
 namespace SourceAFIS.FingerprintAnalysis
 {
-    public class Options
+    public class Options : INotifyPropertyChanged
     {
-        public string ProbePath { get; set; }
-        public string CandidatePath { get; set; }
+        string ProbePathValue = "";
+        public string ProbePath
+        {
+            get { return ProbePathValue; }
+            set { ProbePathValue = value; OnPropertyChanged("ProbePath"); }
+        }
+
+        string CandidatePathValue = "";
+        public string CandidatePath
+        {
+            get { return CandidatePathValue; }
+            set { CandidatePathValue = value; OnPropertyChanged("CandidatePath"); }
+        }
+
         public LayerType DisplayLayer { get; set; }
         public SkeletonType SkeletonType { get; set; }
         public QuickCompare CompareWith { get; set; }
@@ -21,5 +34,13 @@ namespace SourceAFIS.FingerprintAnalysis
         public bool Orientation { get; set; }
         public bool MinutiaCollector { get; set; }
         public bool PairedMinutiae { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
     }
 }
