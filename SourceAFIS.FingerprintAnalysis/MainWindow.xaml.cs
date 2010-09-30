68dfda6627d2e65a4885b694d62864e0baa53444
robertvanak <robertvanak@localhost>
2010-09-30 22:35:12 +0200

    Save/restore options

--- SourceAFIS.FingerprintAnalysis/MainWindow.xaml.cs
+++ SourceAFIS.FingerprintAnalysis/MainWindow.xaml.cs
@@ -13,6 +13,9 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.ComponentModel;
 using System.IO;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using System.Reflection;
 using Microsoft.Win32;
 using SourceAFIS.General;
 
@@ -20,6 +23,10 @@ namespace SourceAFIS.FingerprintAnalysis
 {
     public partial class MainWindow : Window
     {
+        static readonly string SettingsPath = System.IO.Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "SourceAFIS", "FingerprintAnalysisSettings.xml");
+        static readonly string ProgramVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
         Options Options;
         LogCollector Collector = new LogCollector();
         Blender Blender = new Blender();
@@ -28,9 +35,11 @@ namespace SourceAFIS.FingerprintAnalysis
         {
             InitializeComponent();
             Options = FindResource("OptionsData") as Options;
+            LoadSettings();
             Blender.Options = Options;
             Blender.Logs = Collector;
             Options.PropertyChanged += (source, args) => { OnOptionsChange(); };
+            OnOptionsChange();
         }
 
         private void LeftOpen_Click(object sender, RoutedEventArgs e)
@@ -73,6 +82,45 @@ namespace SourceAFIS.FingerprintAnalysis
 
                 LeftImage.Source = converted;
             }
+        }
+
+        void LoadSettings()
+        {
+            try
+            {
+                XDocument xml = XDocument.Load(SettingsPath);
+                if ((string)xml.Root.Attribute("ProgramVersion") == ProgramVersion)
+                {
+                    xml.Root.Attribute("ProgramVersion").Remove();
+
+                    MemoryStream unversioned = new MemoryStream();
+                    xml.Save(unversioned);
+                    unversioned.Close();
+
+                    XmlSerializer serializer = new XmlSerializer(typeof(Options));
+                    Options loaded = serializer.Deserialize(new MemoryStream(unversioned.GetBuffer())) as Options;
+
+                    foreach (PropertyInfo property in typeof(Options).GetProperties())
+                        property.SetValue(Options, property.GetValue(loaded, null), null);
+                }
+            }
+            catch
+            {
+            }
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            MemoryStream unversioned = new MemoryStream();
+            XmlSerializer serializer = new XmlSerializer(typeof(Options));
+            serializer.Serialize(unversioned, Options);
+            unversioned.Close();
+
+            XDocument xml = XDocument.Load(new MemoryStream(unversioned.GetBuffer()));
+            xml.Root.SetAttributeValue("ProgramVersion", ProgramVersion);
+
+            Directory.CreateDirectory(System.IO.Path.GetDirectoryName(SettingsPath));
+            xml.Save(SettingsPath);
         }
     }
 }
