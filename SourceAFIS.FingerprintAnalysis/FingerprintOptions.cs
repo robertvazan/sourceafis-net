26a796d42ae7394f7a46e1b64677212c62f2417b
robertvanak <robertvanak@localhost>
2010-10-03 21:57:46 +0200

    Symmetric options (just path) in separate class

---
+++ SourceAFIS.FingerprintAnalysis/FingerprintOptions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace SourceAFIS.FingerprintAnalysis
+{
+    public class FingerprintOptions : INotifyPropertyChanged
+    {
+        string PathValue = "";
+        public string Path
+        {
+            get { return PathValue; }
+            set { PathValue = value; OnPropertyChanged("Path"); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
