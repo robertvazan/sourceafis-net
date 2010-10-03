de611704bc14e1d7fd1050898f45a7485f682cf7
robertvanak <robertvanak@localhost>
2010-10-03 20:25:14 +0200

    Moved enums into Options class

--- SourceAFIS.FingerprintAnalysis/Blender.cs
+++ SourceAFIS.FingerprintAnalysis/Blender.cs
@@ -94,7 +94,7 @@ namespace SourceAFIS.FingerprintAnalysis
             LogCollector.SkeletonData skeletonData = GetSkeletonData(data);
             if (Options.EnableImageDisplay)
             {
-                LayerType displayLayerType = Options.DisplayLayer;
+                Options.LayerType displayLayerType = Options.DisplayLayer;
                 float[,] displayLayer = GlobalContrast.GetNormalized(GetLayer(displayLayerType, data, skeletonData));
                 return ScalarColoring.Interpolate(GlobalContrast.GetNormalized(displayLayer), ColorF.Transparent, palette.Image);
             }
@@ -107,21 +107,21 @@ namespace SourceAFIS.FingerprintAnalysis
             if (Options.EnableImageDisplay)
             {
                 LogCollector.SkeletonData skeletonData = GetSkeletonData(data);
-                LayerType displayLayerType = Options.DisplayLayer;
-                LayerType compareLayerType = displayLayerType;
-                if (Options.CompareWith != QuickCompare.None)
+                Options.LayerType displayLayerType = Options.DisplayLayer;
+                Options.LayerType compareLayerType = displayLayerType;
+                if (Options.CompareWith != Options.QuickCompare.None)
                 {
-                    if (Options.CompareWith == QuickCompare.OtherLayer)
+                    if (Options.CompareWith == Options.QuickCompare.OtherLayer)
                         compareLayerType = Options.CompareWithLayer;
                     else
                     {
                         int compareLayerIndex;
-                        if (Options.CompareWith == QuickCompare.Next)
+                        if (Options.CompareWith == Options.QuickCompare.Next)
                             compareLayerIndex = (int)displayLayerType + 1;
                         else
                             compareLayerIndex = (int)displayLayerType - 1;
-                        if (Enum.IsDefined(typeof(LayerType), compareLayerIndex))
-                            compareLayerType = (LayerType)Enum.Parse(typeof(LayerType), compareLayerIndex.ToString());
+                        if (Enum.IsDefined(typeof(Options.LayerType), compareLayerIndex))
+                            compareLayerType = (Options.LayerType)Enum.Parse(typeof(Options.LayerType), compareLayerIndex.ToString());
                     }
                 }
 
@@ -134,11 +134,11 @@ namespace SourceAFIS.FingerprintAnalysis
                         diff = ImageDiff.Diff(compareLayer, displayLayer);
                     else
                         diff = ImageDiff.Diff(displayLayer, compareLayer);
-                    if (Options.DiffType == DiffType.Normalized)
+                    if (Options.DiffMode == Options.DiffType.Normalized)
                         diff = ImageDiff.Normalize(diff, 10);
-                    if (Options.DiffType == DiffType.Fog)
+                    if (Options.DiffMode == Options.DiffType.Fog)
                         diff = ImageDiff.Binarize(diff, 0.05f, 0.5f);
-                    if (Options.DiffType == DiffType.Binary)
+                    if (Options.DiffMode == Options.DiffType.Binary)
                         diff = ImageDiff.Binarize(diff, 0.05f, 1);
                     return ImageDiff.Render(diff);
                 }
@@ -171,9 +171,9 @@ namespace SourceAFIS.FingerprintAnalysis
         ColorF[,] BlendMask(LogCollector.ExtractionData data, Palette palette)
         {
             BinaryMap mask = null;
-            if (Options.Mask == MaskType.Segmentation)
+            if (Options.Mask == Options.MaskType.Segmentation)
                 mask = BlockFiller.FillBlocks(data.SegmentationMask.GetInverted(), data.Blocks);
-            if (Options.Mask == MaskType.Inner)
+            if (Options.Mask == Options.MaskType.Inner)
                 mask = data.InnerMask.GetInverted();
             if (mask != null)
                 return ScalarColoring.Mask(mask, ColorF.Transparent, LightFog);
@@ -188,32 +188,32 @@ namespace SourceAFIS.FingerprintAnalysis
 
         LogCollector.SkeletonData GetSkeletonData(LogCollector.ExtractionData data)
         {
-            if (Options.SkeletonType == SkeletonType.Ridges)
+            if (Options.SelectedSkeleton == Options.SkeletonType.Ridges)
                 return data.Ridges;
             else
                 return data.Valleys;
         }
 
-        float[,] GetLayer(LayerType type, LogCollector.ExtractionData data, LogCollector.SkeletonData skeleton)
+        float[,] GetLayer(Options.LayerType type, LogCollector.ExtractionData data, LogCollector.SkeletonData skeleton)
         {
             switch (type)
             {
-                case LayerType.OriginalImage: return GrayscaleInverter.GetInverted(PixelFormat.ToFloat(data.InputImage));
-                case LayerType.Equalized: return data.Equalized;
-                case LayerType.SmoothedRidges: return data.SmoothedRidges;
-                case LayerType.OrthogonalSmoothing: return data.OrthogonalSmoothing;
-                case LayerType.Binarized: return PixelFormat.ToFloat(data.Binarized);
-                case LayerType.BinarySmoothing: return PixelFormat.ToFloat(data.BinarySmoothing);
-                case LayerType.RemovedCrosses: return PixelFormat.ToFloat(data.RemovedCrosses);
-                case LayerType.Thinned: return PixelFormat.ToFloat(skeleton.Thinned);
-                case LayerType.RidgeTracer: return PixelFormat.ToFloat(SkeletonDrawer.Draw(skeleton.RidgeTracer, data.Binarized.Size));
-                case LayerType.DotRemover: return PixelFormat.ToFloat(SkeletonDrawer.Draw(skeleton.DotRemover, data.Binarized.Size));
-                case LayerType.PoreRemover: return PixelFormat.ToFloat(SkeletonDrawer.Draw(skeleton.PoreRemover, data.Binarized.Size));
-                case LayerType.GapRemover: return PixelFormat.ToFloat(SkeletonDrawer.Draw(skeleton.GapRemover, data.Binarized.Size));
-                case LayerType.TailRemover: return PixelFormat.ToFloat(SkeletonDrawer.Draw(skeleton.TailRemover, data.Binarized.Size));
-                case LayerType.FragmentRemover: return PixelFormat.ToFloat(SkeletonDrawer.Draw(skeleton.FragmentRemover, data.Binarized.Size));
-                case LayerType.MinutiaMask: return PixelFormat.ToFloat(SkeletonDrawer.Draw(skeleton.MinutiaMask, data.Binarized.Size));
-                case LayerType.BranchMinutiaRemover: return PixelFormat.ToFloat(SkeletonDrawer.Draw(skeleton.BranchMinutiaRemover, data.Binarized.Size));
+                case Options.LayerType.OriginalImage: return GrayscaleInverter.GetInverted(PixelFormat.ToFloat(data.InputImage));
+                case Options.LayerType.Equalized: return data.Equalized;
+                case Options.LayerType.SmoothedRidges: return data.SmoothedRidges;
+                case Options.LayerType.OrthogonalSmoothing: return data.OrthogonalSmoothing;
+                case Options.LayerType.Binarized: return PixelFormat.ToFloat(data.Binarized);
+                case Options.LayerType.BinarySmoothing: return PixelFormat.ToFloat(data.BinarySmoothing);
+                case Options.LayerType.RemovedCrosses: return PixelFormat.ToFloat(data.RemovedCrosses);
+                case Options.LayerType.Thinned: return PixelFormat.ToFloat(skeleton.Thinned);
+                case Options.LayerType.RidgeTracer: return PixelFormat.ToFloat(SkeletonDrawer.Draw(skeleton.RidgeTracer, data.Binarized.Size));
+                case Options.LayerType.DotRemover: return PixelFormat.ToFloat(SkeletonDrawer.Draw(skeleton.DotRemover, data.Binarized.Size));
+                case Options.LayerType.PoreRemover: return PixelFormat.ToFloat(SkeletonDrawer.Draw(skeleton.PoreRemover, data.Binarized.Size));
+                case Options.LayerType.GapRemover: return PixelFormat.ToFloat(SkeletonDrawer.Draw(skeleton.GapRemover, data.Binarized.Size));
+                case Options.LayerType.TailRemover: return PixelFormat.ToFloat(SkeletonDrawer.Draw(skeleton.TailRemover, data.Binarized.Size));
+                case Options.LayerType.FragmentRemover: return PixelFormat.ToFloat(SkeletonDrawer.Draw(skeleton.FragmentRemover, data.Binarized.Size));
+                case Options.LayerType.MinutiaMask: return PixelFormat.ToFloat(SkeletonDrawer.Draw(skeleton.MinutiaMask, data.Binarized.Size));
+                case Options.LayerType.BranchMinutiaRemover: return PixelFormat.ToFloat(SkeletonDrawer.Draw(skeleton.BranchMinutiaRemover, data.Binarized.Size));
                 default: throw new AssertException();
             }
         }
