822d00aad6df3c9f919e9a7bcfef4afbb7a9234a
Robert Vazan <robvcode@gmail.com>
2010-10-08 17:15:30 +0200

    Public property metadata

--- SourceAFIS.FingerprintAnalysis/ExtractionData.cs
+++ SourceAFIS.FingerprintAnalysis/ExtractionData.cs
@@ -17,55 +17,55 @@ namespace SourceAFIS.FingerprintAnalysis
         public SkeletonData Ridges = new SkeletonData();
         public SkeletonData Valleys = new SkeletonData();
 
-        LogProperty InputImageProperty = new LogProperty();
+        public LogProperty InputImageProperty = new LogProperty();
         public byte[,] InputImage { get { return (byte[,])InputImageProperty.Value; } }
 
-        LogProperty BlocksProperty = new LogProperty();
+        public LogProperty BlocksProperty = new LogProperty();
         public BlockMap Blocks { get { return (BlockMap)BlocksProperty.Value; } }
 
-        LogProperty BlockContrastProperty = new LogProperty();
+        public LogProperty BlockContrastProperty = new LogProperty();
         public byte[,] BlockContrast { get { return (byte[,])BlockContrastProperty.Value; } }
 
-        LogProperty AbsoluteContrastProperty = new LogProperty();
+        public LogProperty AbsoluteContrastProperty = new LogProperty();
         public BinaryMap AbsoluteContrast { get { return (BinaryMap)AbsoluteContrastProperty.Value; } }
 
-        LogProperty RelativeContrastProperty = new LogProperty();
+        public LogProperty RelativeContrastProperty = new LogProperty();
         public BinaryMap RelativeContrast { get { return (BinaryMap)RelativeContrastProperty.Value; } }
 
-        LogProperty LowContrastMajorityProperty = new LogProperty();
+        public LogProperty LowContrastMajorityProperty = new LogProperty();
         public BinaryMap LowContrastMajority { get { return (BinaryMap)LowContrastMajorityProperty.Value; } }
 
-        LogProperty SegmentationMaskProperty = new LogProperty();
+        public LogProperty SegmentationMaskProperty = new LogProperty();
         public BinaryMap SegmentationMask { get { return (BinaryMap)SegmentationMaskProperty.Value; } }
 
-        LogProperty EqualizedProperty = new LogProperty();
+        public LogProperty EqualizedProperty = new LogProperty();
         public float[,] Equalized { get { return (float[,])EqualizedProperty.Value; } }
 
-        LogProperty OrientationProperty = new LogProperty();
+        public LogProperty OrientationProperty = new LogProperty();
         public byte[,] Orientation { get { return (byte[,])OrientationProperty.Value; } }
 
-        LogProperty SmoothedRidgesProperty = new LogProperty();
+        public LogProperty SmoothedRidgesProperty = new LogProperty();
         public float[,] SmoothedRidges { get { return (float[,])SmoothedRidgesProperty.Value; } }
 
-        LogProperty OrthogonalSmoothingProperty = new LogProperty();
+        public LogProperty OrthogonalSmoothingProperty = new LogProperty();
         public float[,] OrthogonalSmoothing { get { return (float[,])OrthogonalSmoothingProperty.Value; } }
 
-        LogProperty BinarizedProperty = new LogProperty();
+        public LogProperty BinarizedProperty = new LogProperty();
         public BinaryMap Binarized { get { return (BinaryMap)BinarizedProperty.Value; } }
 
-        LogProperty BinarySmoothingProperty = new LogProperty();
+        public LogProperty BinarySmoothingProperty = new LogProperty();
         public BinaryMap BinarySmoothing { get { return (BinaryMap)BinarySmoothingProperty.Value; } }
 
-        LogProperty RemovedCrossesProperty = new LogProperty();
+        public LogProperty RemovedCrossesProperty = new LogProperty();
         public BinaryMap RemovedCrosses { get { return (BinaryMap)RemovedCrossesProperty.Value; } }
 
-        LogProperty InnerMaskProperty = new LogProperty();
+        public LogProperty InnerMaskProperty = new LogProperty();
         public BinaryMap InnerMask { get { return (BinaryMap)InnerMaskProperty.Value; } }
 
-        LogProperty MinutiaCollectorProperty = new LogProperty();
+        public LogProperty MinutiaCollectorProperty = new LogProperty();
         public TemplateBuilder MinutiaCollector { get { return (TemplateBuilder)MinutiaCollectorProperty.Value; } }
 
-        ComputedProperty TemplateProperty = new ComputedProperty("MinutiaCollector");
+        public ComputedProperty TemplateProperty = new ComputedProperty("MinutiaCollector");
         public Template Template { get { return new SerializedFormat().Export(MinutiaCollector); } }
     }
 }
--- SourceAFIS.FingerprintAnalysis/SkeletonData.cs
+++ SourceAFIS.FingerprintAnalysis/SkeletonData.cs
@@ -14,34 +14,34 @@ namespace SourceAFIS.FingerprintAnalysis
             RegisterProperties();
         }
 
-        LogProperty BinarizedProperty = new LogProperty();
+        public LogProperty BinarizedProperty = new LogProperty();
         public BinaryMap Binarized { get { return (BinaryMap)BinarizedProperty.Value; } }
 
-        LogProperty ThinnedProperty = new LogProperty();
+        public LogProperty ThinnedProperty = new LogProperty();
         public BinaryMap Thinned { get { return (BinaryMap)ThinnedProperty.Value; } }
 
-        LogProperty RidgeTracerProperty = new LogProperty();
+        public LogProperty RidgeTracerProperty = new LogProperty();
         public SkeletonBuilder RidgeTracer { get { return (SkeletonBuilder)RidgeTracerProperty.Value; } }
 
-        LogProperty DotRemoverProperty = new LogProperty();
+        public LogProperty DotRemoverProperty = new LogProperty();
         public SkeletonBuilder DotRemover { get { return (SkeletonBuilder)DotRemoverProperty.Value; } }
 
-        LogProperty PoreRemoverProperty = new LogProperty();
+        public LogProperty PoreRemoverProperty = new LogProperty();
         public SkeletonBuilder PoreRemover { get { return (SkeletonBuilder)PoreRemoverProperty.Value; } }
 
-        LogProperty GapRemoverProperty = new LogProperty();
+        public LogProperty GapRemoverProperty = new LogProperty();
         public SkeletonBuilder GapRemover { get { return (SkeletonBuilder)GapRemoverProperty.Value; } }
 
-        LogProperty TailRemoverProperty = new LogProperty();
+        public LogProperty TailRemoverProperty = new LogProperty();
         public SkeletonBuilder TailRemover { get { return (SkeletonBuilder)TailRemoverProperty.Value; } }
 
-        LogProperty FragmentRemoverProperty = new LogProperty();
+        public LogProperty FragmentRemoverProperty = new LogProperty();
         public SkeletonBuilder FragmentRemover { get { return (SkeletonBuilder)FragmentRemoverProperty.Value; } }
 
-        LogProperty MinutiaMaskProperty = new LogProperty();
+        public LogProperty MinutiaMaskProperty = new LogProperty();
         public SkeletonBuilder MinutiaMask { get { return (SkeletonBuilder)MinutiaMaskProperty.Value; } }
 
-        LogProperty BranchMinutiaRemoverProperty = new LogProperty();
+        public LogProperty BranchMinutiaRemoverProperty = new LogProperty();
         public SkeletonBuilder BranchMinutiaRemover { get { return (SkeletonBuilder)BranchMinutiaRemoverProperty.Value; } }
     }
 }
