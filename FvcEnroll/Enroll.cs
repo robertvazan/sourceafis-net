d6b3c5a315973489ab30586bb6560ec9565312be
Robert Vazan <robvcode@gmail.com>
2010-10-21 22:26:46 +0200

    Use AsBitmapImage in FVC submission

--- FvcEnroll/Enroll.cs
+++ FvcEnroll/Enroll.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.IO;
-using System.Drawing;
+using System.Windows.Media.Imaging;
 using SourceAFIS.Simple;
 
 namespace FvcEnroll
@@ -24,11 +24,7 @@ namespace FvcEnroll
             {
                 AfisEngine afis = new AfisEngine();
                 Fingerprint fp = new Fingerprint();
-                using (Image image = Image.FromFile(args[0]))
-                {
-                    using (Bitmap bitmap = new Bitmap(image))
-                        fp.AsBitmap = bitmap;
-                }
+                fp.AsBitmapSource = new BitmapImage(new Uri(args[0], UriKind.RelativeOrAbsolute));
                 afis.Extract(fp);
                 File.WriteAllBytes(args[1], fp.Template);
                 WriteLog(args, "OK");
