d8c6837733a6d5894ec957f3de6ef0d84764bd18
robertvanak <robertvanak@localhost>
2011-01-07 11:42:51 +0100

    Test options persistency

--- SourceAFIS.Tests/FingerprintAnalysis/Startup.cs
+++ SourceAFIS.Tests/FingerprintAnalysis/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using System.Xml.Linq;
 using NUnit.Framework;
 using White.Core;
 using White.Core.Factory;
@@ -13,14 +14,6 @@ namespace SourceAFIS.Tests.FingerprintAnalysis
     [TestFixture]
     public class Startup : Common
     {
-        [Test]
-        public void DefaultStartup()
-        {
-            StartApp();
-            Assert.IsNotNull(Win);
-            Assert.IsTrue(Win.DisplayState == DisplayState.Maximized);
-        }
-
         public override void TestFixtureSetUp()
         {
         }
@@ -32,6 +25,79 @@ namespace SourceAFIS.Tests.FingerprintAnalysis
                 base.TearDown();
                 base.TestFixtureTearDown();
             }
+        }
+
+        [Test]
+        public void DefaultStartup()
+        {
+            StartApp();
+            Assert.IsNotNull(Win);
+            Assert.IsTrue(Win.DisplayState == DisplayState.Maximized);
+        }
+
+        [Test]
+        public void SavedOptions()
+        {
+            StartApp();
+            CloseFiles();
+            ResetOptions();
+            CloseApp();
+
+            StartApp();
+            Assert.AreEqual("", Left.FileName.Text);
+            Assert.AreEqual("", Right.FileName.Text);
+            Assert.AreEqual("OriginalImage", LayerChoice.GetSelectedItemText());
+            Assert.AreEqual("Ridges", SkeletonChoice.GetSelectedItemText());
+            Assert.AreEqual("None", MaskChoice.GetSelectedItemText());
+            Assert.AreEqual(false, Contrast.Checked);
+            Assert.AreEqual(false, Orientation.Checked);
+            Assert.AreEqual(false, Minutiae.Checked);
+            Assert.AreEqual(false, Paired.Checked);
+
+            SelectFiles();
+            FullOptions();
+            CloseApp();
+
+            StartApp();
+            Assert.AreEqual(Path.GetFileNameWithoutExtension(Settings.SomeFingerprintPath), Left.FileName.Text);
+            Assert.AreEqual(Path.GetFileNameWithoutExtension(Settings.MatchingFingerprintPath), Right.FileName.Text);
+            Assert.AreEqual("MinutiaMask", LayerChoice.GetSelectedItemText());
+            Assert.AreEqual("Valleys", SkeletonChoice.GetSelectedItemText());
+            Assert.AreEqual("Segmentation", MaskChoice.GetSelectedItemText());
+            Assert.AreEqual(true, Contrast.Checked);
+            Assert.AreEqual(true, Orientation.Checked);
+            Assert.AreEqual(true, Minutiae.Checked);
+            Assert.AreEqual(true, Paired.Checked);
+        }
+
+        [Test]
+        public void VersionChange()
+        {
+            StartApp();
+            SelectFiles();
+            FullOptions();
+            CloseApp();
+
+            string path = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+                "SourceAFIS", "FingerprintAnalysisSettings.xml");
+            XDocument xml = XDocument.Load(path);
+            xml.Root.Attribute("ProgramVersion").SetValue("invalid");
+            xml.Save(path);
+
+            StartApp();
+            Assert.AreEqual("", Left.FileName.Text);
+            Assert.AreEqual("", Right.FileName.Text);
+            Assert.AreEqual("OriginalImage", LayerChoice.GetSelectedItemText());
+            Assert.AreEqual("Ridges", SkeletonChoice.GetSelectedItemText());
+            Assert.AreEqual("None", MaskChoice.GetSelectedItemText());
+            Assert.AreEqual(false, Contrast.Checked);
+            Assert.AreEqual(false, Orientation.Checked);
+            Assert.AreEqual(false, Minutiae.Checked);
+            Assert.AreEqual(false, Paired.Checked);
+
+            CloseApp();
+            xml = XDocument.Load(path);
+            Assert.AreNotEqual("invalid", (string)xml.Root.Attribute("ProgramVersion"));
         }
     }
 }
