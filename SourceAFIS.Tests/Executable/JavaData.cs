071eddb7b72dd8ffe71b4ee8bfc1f3c1ca6c5280
Robert Vazan <robvcode@gmail.com>
2011-08-24 21:06:09 +0200

    Separate special test case to generate templates for Java tests

--- SourceAFIS.Tests/Executable/JavaData.cs
+++ SourceAFIS.Tests/Executable/JavaData.cs
@@ -16,26 +16,55 @@ namespace SourceAFIS.Tests.Executable
     [Category("Special")]
     class JavaData
     {
+        void ClipDatabase(DatabaseCollection db)
+        {
+            db.ClipDatabaseCount(3);
+            db.ClipFingersPerDatabase(3);
+            db.ClipViewsPerFinger(3);
+        }
+
         Person ToPerson(Template template)
         {
             return new Person(new Fingerprint() { Template = new CompactFormat().SerializeBuilder(new SerializedFormat().Import(template)) });
         }
 
         [Test]
-        public void Scores()
+        public void Templates()
         {
-            XDocument document = new XDocument(new XElement("score-list"));
+            XDocument document = new XDocument(new XElement("template-list"));
             AfisEngine afis = new AfisEngine();
             DatabaseCollection db = new DatabaseCollection();
             db.Scan(Settings.DatabasePath);
+            ClipDatabase(db);
             foreach (var database in db.Databases)
-            {
                 foreach (var index in database.AllIndexes)
                 {
                     Fingerprint fp = new Fingerprint();
                     fp.AsBitmapSource = new BitmapImage(new Uri(database[index].FilePath, UriKind.RelativeOrAbsolute));
                     afis.Extract(new Person(fp));
-                    database[index].Template = new SerializedFormat().Export(new CompactFormat().Import(fp.Template));
+                    document.Root.Add(new XElement("template",
+                        new XAttribute("image-path", database[index].FilePath),
+                        new XAttribute("compact", Convert.ToBase64String(fp.Template))));
+                }
+            Directory.CreateDirectory(Settings.JavaDataPath);
+            document.Save(Path.Combine(Settings.JavaDataPath, "templates.xml"));
+        }
+
+        [Test]
+        public void Scores()
+        {
+            var templates = XDocument.Load(Path.Combine(Settings.JavaDataPath, "templates.xml")).Root.Elements();
+            XDocument document = new XDocument(new XElement("score-list"));
+            AfisEngine afis = new AfisEngine();
+            DatabaseCollection db = new DatabaseCollection();
+            db.Scan(Settings.DatabasePath);
+            ClipDatabase(db);
+            foreach (var database in db.Databases)
+            {
+                foreach (var index in database.AllIndexes)
+                {
+                    var template = (string)templates.First(t => (string)t.Attribute("image-path") == database[index].FilePath).Attribute("compact");
+                    database[index].Template = new SerializedFormat().Export(new CompactFormat().Import(Convert.FromBase64String(template)));
                 }
                 foreach (var pair in database.AllPairs)
                 {
@@ -46,7 +75,6 @@ namespace SourceAFIS.Tests.Executable
                         new XAttribute("score", score.ToString("0.####"))));
                 }
             }
-            Directory.CreateDirectory(Settings.JavaDataPath);
             document.Save(Path.Combine(Settings.JavaDataPath, "score.xml"));
         }
     }
