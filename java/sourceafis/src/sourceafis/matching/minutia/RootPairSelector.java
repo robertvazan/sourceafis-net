be2964cc81b913c34717827b60001e01def8a843
Robert Vazan <robvcode@gmail.com>
2011-08-28 13:11:53 +0200

    Lazily evaluated RootPairSelector

--- java/sourceafis/src/sourceafis/matching/minutia/RootPairSelector.java
+++ java/sourceafis/src/sourceafis/matching/minutia/RootPairSelector.java
@@ -1,17 +1,44 @@
 package sourceafis.matching.minutia;
 
-import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
 import sourceafis.extraction.templates.Template;
 
-public class RootPairSelector {
-	public ArrayList<MinutiaPair> GetRoots(Template probeTemplate, Template candidateTemplate)
-    {
-	 	ArrayList<MinutiaPair> list=new ArrayList<MinutiaPair>();
-        for (int probe = 0; probe < probeTemplate.Minutiae.length; ++probe)
-           for (int candidate = 0; candidate < candidateTemplate.Minutiae.length; ++candidate) {
-               int mixedProbe = (probe + candidate) % probeTemplate.Minutiae.length;
-        	   list.add(new MinutiaPair(mixedProbe, candidate));
-           }
-         return list;
-    }
+public class RootPairSelector implements Iterable<MinutiaPair>, Iterator<MinutiaPair> {
+	Template probeTemplate;
+	Template candidateTemplate;
+	int probe;
+	int candidate;
+
+	public Iterable<MinutiaPair> getRoots(Template p, Template c) {
+		probeTemplate = p;
+		candidateTemplate = c;
+		probe = 0;
+		candidate = 0;
+		return this;
+	}
+	
+	public Iterator<MinutiaPair> iterator() { return this; }
+	public void remove() { throw new UnsupportedOperationException(); }
+
+	public boolean hasNext() {
+		return probe < probeTemplate.Minutiae.length && candidate < candidateTemplate.Minutiae.length;
+	}
+
+	public MinutiaPair next() {
+		if (!hasNext())
+			throw new NoSuchElementException();
+
+		int mixedProbe = (probe + candidate) % probeTemplate.Minutiae.length;
+		MinutiaPair result = new MinutiaPair(mixedProbe, candidate);
+		
+		++candidate;
+		if (candidate >= candidateTemplate.Minutiae.length) {
+			++probe;
+			candidate = 0;
+		}
+		
+		return result;
+	}
 }
