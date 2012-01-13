b2c1df6ea6946bb8f7221395b45d163121e918b0
Robert Vazan <robvcode@gmail.com>
2012-01-13 13:56:32 +0100

    Avoid parameter with empty range

--- SourceAFIS/Extraction/Minutiae/StandardDpiScaling.cs
+++ SourceAFIS/Extraction/Minutiae/StandardDpiScaling.cs
@@ -11,7 +11,6 @@ namespace SourceAFIS.Extraction.Minutiae
     public sealed class StandardDpiScaling
     {
         [DpiAdjusted]
-        [Parameter(Lower = 500, Upper = 500)]
         public int DpiScaling = 500;
 
         public DetailLogger.Hook Logger = DetailLogger.Null;
