850f024d8575b347bdbbe949fef09c5b9442079f
Robert Vazan <robvcode@gmail.com>
2012-01-13 21:11:59 +0100

    Separate bitmap and marker layers

---
+++ SourceAFIS.FingerprintAnalysis/MarkerLayer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SourceAFIS.FingerprintAnalysis
+{
+    public enum MarkerLayer
+    {
+        None,
+        Thinned,
+        RidgeTracer,
+        DotRemover,
+        PoreRemover,
+        GapRemover,
+        TailRemover,
+        FragmentRemover,
+        BranchMinutiaRemover,
+        MinutiaCollector,
+        MinutiaMask
+    }
+}
