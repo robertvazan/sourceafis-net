776a5763772915c9f1ed1b7309dc5b9ddde6c2f0
robert-vazan <robert-vazan@localhost>
2012-01-22 00:54:24 +0100

    Optimized parameters

--- SourceAFIS/Matching/Minutia/EdgeTable.cs
+++ SourceAFIS/Matching/Minutia/EdgeTable.cs
@@ -13,7 +13,7 @@ namespace SourceAFIS.Matching.Minutia
         public EdgeConstructor EdgeConstructor = new EdgeConstructor();
 
         [Parameter(Lower = 30, Upper = 1500)]
-        public int MaxDistance = 1150;
+        public int MaxDistance = 550;
         [Parameter(Lower = 2, Upper = 100)]
         public int MaxNeighbors = 9;
 
