88f3aefd3f9fd229323f2bd075c8f7068f5427b4
robert-vazan <robert-vazan@localhost>
2012-01-28 02:02:58 +0100

    Test runner project for easier debugging

---
+++ SourceAFIS.Tests.Runner/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace SourceAFIS.Tests.Runner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NUnit.ConsoleRunner.Runner.Main(new string[]
+            {
+                typeof(SourceAFIS.Tests.Executable.Installer).Assembly.Location
+            });
+        }
+    }
+}
