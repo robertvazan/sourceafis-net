cb783284fcc6d8303b9a90ce681d7629d7034aff
robert-vazan <robert-vazan@localhost>
2012-03-28 09:48:12 +0200

    Cleanup in java tests

---
+++ java/sourceafis/test/sourceafis/general/TestSettings.java
@@ -0,0 +1,14 @@
+package sourceafis.general;
+
+import java.io.File;
+
+public class TestSettings {
+	public static final File folderJavaProject = new File(System.getProperty("user.dir"));
+	public static final File folderRoot = new File(new File(folderJavaProject, ".."), "..");
+	public static final File folderJavaTestData = new File(new File(folderRoot, "Data"), "JavaTestData");
+	
+	public static final File fileTemplates = new File(folderJavaTestData, "templates.xml");
+	public static final File fileScore = new File(folderJavaTestData, "score.xml");
+	public static final File fileMatcherLog = new File(folderJavaTestData, "matcher.xml");
+	public static final File fileParameters = new File(folderJavaTestData, "parameters.xml");
+}
