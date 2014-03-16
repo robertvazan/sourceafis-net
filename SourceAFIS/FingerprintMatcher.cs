e9b653a55194943b7076dcb1353f0da1d27918c4
Robert Vazan <robvcode@gmail.com>
2014-03-16 22:29:41 +0100

    Integrated edge hash

--- SourceAFIS/FingerprintMatcher.cs
+++ SourceAFIS/FingerprintMatcher.cs
@@ -18,13 +18,13 @@ namespace SourceAFIS
         const int MaxTriedTriangles = 7538;
 
         internal FingerprintTemplate Template;
-        internal EdgeHash EdgeHash;
+        Dictionary<int, List<IndexedEdge>> EdgeHash = new Dictionary<int, List<IndexedEdge>>();
         FingerprintTemplate Candidate;
 
         public FingerprintMatcher(FingerprintTemplate template)
         {
             Template = template;
-            EdgeHash = new EdgeHash(template);
+            BuildEdgeHash();
         }
 
         public double Match(FingerprintTemplate candidate)
@@ -52,12 +52,55 @@ namespace SourceAFIS
             return bestScore;
         }
 
+        class IndexedEdge
+        {
+            public EdgeShape Shape;
+            public EdgeLocation Location;
+        }
+
+        void BuildEdgeHash()
+        {
+            for (int referenceMinutia = 0; referenceMinutia < Template.Minutiae.Count; ++referenceMinutia)
+                for (int neighborMinutia = 0; neighborMinutia < Template.Minutiae.Count; ++neighborMinutia)
+                    if (referenceMinutia != neighborMinutia)
+                    {
+                        var edge = new IndexedEdge()
+                        {
+                            Shape = new EdgeShape(Template, referenceMinutia, neighborMinutia),
+                            Location = new EdgeLocation(referenceMinutia, neighborMinutia)
+                        };
+                        foreach (var hash in GetShapeCoverage(edge.Shape))
+                        {
+                            List<IndexedEdge> list;
+                            if (!EdgeHash.TryGetValue(hash, out list))
+                                EdgeHash[hash] = list = new List<IndexedEdge>();
+                            list.Add(edge);
+                        }
+                    }
+        }
+
+        static IEnumerable<int> GetShapeCoverage(EdgeShape edge)
+        {
+            int minLengthBin = (edge.Length - MaxDistanceError) / MaxDistanceError;
+            int maxLengthBin = (edge.Length + MaxDistanceError) / MaxDistanceError;
+            int angleBins = MathEx.DivRoundUp(256, MaxAngleError);
+            int minReferenceBin = Angle.Difference(edge.ReferenceAngle, MaxAngleError) / MaxAngleError;
+            int maxReferenceBin = Angle.Add(edge.ReferenceAngle, MaxAngleError) / MaxAngleError;
+            int endReferenceBin = (maxReferenceBin + 1) % angleBins;
+            int minNeighborBin = Angle.Difference(edge.NeighborAngle, MaxAngleError) / MaxAngleError;
+            int maxNeighborBin = Angle.Add(edge.NeighborAngle, MaxAngleError) / MaxAngleError;
+            int endNeighborBin = (maxNeighborBin + 1) % angleBins;
+            for (int lengthBin = minLengthBin; lengthBin <= maxLengthBin; ++lengthBin)
+                for (int referenceBin = minReferenceBin; referenceBin != endReferenceBin; referenceBin = (referenceBin + 1) % angleBins)
+                    for (int neighborBin = minNeighborBin; neighborBin != endNeighborBin; neighborBin = (neighborBin + 1) % angleBins)
+                        yield return (referenceBin << 24) + (neighborBin << 16) + lengthBin;
+        }
+
         IEnumerable<MinutiaPair> GetRoots()
         {
             const int minEdgeLength = 58;
             const int maxEdgeLookups = 1633;
 
-            var hash = new HashLookup(EdgeHash);
             int counter = 0;
             var filters = new Predicate<EdgeShape>[]
             {
@@ -74,17 +117,46 @@ namespace SourceAFIS
                             var candidateEdge = new EdgeShape(Candidate, candidateReference, candidateNeighbor);
                             if (shapeFilter(candidateEdge))
                             {
-                                for (var match = hash.Select(candidateEdge); match != null; match = hash.Next())
+                                List<IndexedEdge> matches;
+                                if (EdgeHash.TryGetValue(HashShape(candidateEdge), out matches))
                                 {
-                                    var pair = new MinutiaPair(match.Location.Reference, candidateReference);
-                                    yield return pair;
-                                    ++counter;
-                                    if (counter >= maxEdgeLookups)
-                                        yield break;
+                                    foreach (var match in matches)
+                                    {
+                                        if (MatchingShapes(match.Shape, candidateEdge))
+                                        {
+                                            var pair = new MinutiaPair(match.Location.Reference, candidateReference);
+                                            yield return pair;
+                                            ++counter;
+                                            if (counter >= maxEdgeLookups)
+                                                yield break;
+                                        }
+                                    }
                                 }
                             }
                         }
             }
+        }
+
+        static int HashShape(EdgeShape edge)
+        {
+            return (edge.ReferenceAngle / MaxAngleError << 24) + (edge.NeighborAngle / MaxAngleError << 16) + edge.Length / MaxDistanceError;
+        }
+
+        static bool MatchingShapes(EdgeShape probe, EdgeShape candidate)
+        {
+            int lengthDelta = probe.Length - candidate.Length;
+            if (lengthDelta >= -MaxDistanceError && lengthDelta <= MaxDistanceError)
+            {
+                byte complementaryAngleError = Angle.Complementary(MaxAngleError);
+                byte referenceDelta = Angle.Difference(probe.ReferenceAngle, candidate.ReferenceAngle);
+                if (referenceDelta <= MaxAngleError || referenceDelta >= complementaryAngleError)
+                {
+                    byte neighborDelta = Angle.Difference(probe.NeighborAngle, candidate.NeighborAngle);
+                    if (neighborDelta <= MaxAngleError || neighborDelta >= complementaryAngleError)
+                        return true;
+                }
+            }
+            return false;
         }
 
         double TryRoot(MinutiaPair root, FingerprintTemplate candidate)
@@ -112,7 +184,7 @@ namespace SourceAFIS
             var reference = Pairing.LastPair.Pair;
             var probeNeighbors = Template.EdgeTable[reference.Probe];
             var candidateNeigbors = Candidate.EdgeTable[reference.Candidate];
-            var matches = EdgeLookup.FindMatchingPairs(probeNeighbors, candidateNeigbors);
+            var matches = FindMatchingPairs(probeNeighbors, candidateNeigbors);
             foreach (var match in matches)
             {
                 var neighbor = match.Pair;
@@ -124,6 +196,48 @@ namespace SourceAFIS
                     Pairing.AddSupportByProbe(neighbor.Probe);
                 }
             }
+        }
+
+        public struct MatchingPair
+        {
+            public MinutiaPair Pair;
+            public int Distance;
+        }
+
+        static List<MatchingPair> FindMatchingPairs(NeighborEdge[] probeStar, NeighborEdge[] candidateStar)
+        {
+            byte complementaryAngleError = Angle.Complementary(FingerprintMatcher.MaxAngleError);
+            var results = new List<MatchingPair>();
+            Range range = new Range();
+
+            for (int candidateIndex = 0; candidateIndex < candidateStar.Length; ++candidateIndex)
+            {
+                NeighborEdge candidateEdge = candidateStar[candidateIndex];
+
+                while (range.Begin < probeStar.Length && probeStar[range.Begin].Edge.Length < candidateEdge.Edge.Length - FingerprintMatcher.MaxDistanceError)
+                    ++range.Begin;
+                if (range.End < range.Begin)
+                    range.End = range.Begin;
+                while (range.End < probeStar.Length && probeStar[range.End].Edge.Length <= candidateEdge.Edge.Length + FingerprintMatcher.MaxDistanceError)
+                    ++range.End;
+
+                for (int probeIndex = range.Begin; probeIndex < range.End; ++probeIndex)
+                {
+                    var probeEdge = probeStar[probeIndex];
+                    byte referenceDiff = Angle.Difference(probeEdge.Edge.ReferenceAngle, candidateEdge.Edge.ReferenceAngle);
+                    if (referenceDiff <= FingerprintMatcher.MaxAngleError || referenceDiff >= complementaryAngleError)
+                    {
+                        byte neighborDiff = Angle.Difference(probeEdge.Edge.NeighborAngle, candidateEdge.Edge.NeighborAngle);
+                        if (neighborDiff <= FingerprintMatcher.MaxAngleError || neighborDiff >= complementaryAngleError)
+                            results.Add(new MatchingPair()
+                            {
+                                Pair = new MinutiaPair(probeEdge.Neighbor, candidateEdge.Neighbor),
+                                Distance = candidateEdge.Edge.Length
+                            });
+                    }
+                }
+            }
+            return results;
         }
 
         double ComputeScore()
