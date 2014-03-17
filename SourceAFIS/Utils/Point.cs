27a0d803f286f5bce5e7450decbd538dec3b9d13
Robert Vazan <robvcode@gmail.com>
2014-03-17 01:50:51 +0100

    Removed BitImage

--- SourceAFIS/Utils/Point.cs
+++ SourceAFIS/Utils/Point.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections;
 
 namespace SourceAFIS.Utils
 {
-    struct Point
+    struct Point : IEnumerable<Point>
     {
         public int X;
         public int Y;
@@ -18,6 +20,15 @@ namespace SourceAFIS.Utils
         public override int GetHashCode() { return X.GetHashCode() + Y.GetHashCode(); }
 
         public override bool Equals(object other) { return other is Point && this == (Point)other; }
+
+        public static Point SizeOf<T>(T[,] array) { return new Point(array.GetLength(1), array.GetLength(0)); }
+
+        public bool Contains(Point inner) { return inner.X >= 0 && inner.Y >= 0 && inner.X < X && inner.Y < Y; }
+
+        public T[,] Allocate<T>() { return new T[Y, X]; }
+        public T Get<T>(T[,] array) { return array[Y, X]; }
+        public T Get<T>(T[,] array, T fallback) { return X >= 0 && Y >= 0 && X < array.GetLength(1) && Y < array.GetLength(0) ? array[Y, X] : fallback; }
+        public void Set<T>(T[,] array, T value) { array[Y, X] = value; }
 
         public static bool operator ==(Point left, Point right)
         {
@@ -42,6 +53,19 @@ namespace SourceAFIS.Utils
         public static Point operator -(Point point)
         {
             return new Point(-point.X, -point.Y);
+        }
+
+        IEnumerator<Point> IEnumerable<Point>.GetEnumerator()
+        {
+            Point point = new Point();
+            for (point.Y = 0; point.Y < Y; ++point.Y)
+                for (point.X = 0; point.X < X; ++point.X)
+                    yield return point;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable<Point>)this).GetEnumerator();
         }
     }
 }
