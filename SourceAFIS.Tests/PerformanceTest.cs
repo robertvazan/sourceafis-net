0f43ea44972eb178610ad7ccf864be5808e18ae3
Robert Vazan <robvcode@gmail.com>
2014-03-30 16:50:18 +0200

    Fixed matching test

--- SourceAFIS.Tests/PerformanceTest.cs
+++ SourceAFIS.Tests/PerformanceTest.cs
@@ -30,11 +30,11 @@ namespace SourceAFIS.Tests
 			var db = Path.Combine("FVC2000", "DB1_B");
 			Console.WriteLine("Matching database {0}", db);
 			var templates = Path.Combine(TestUtils.TemplatePath, db);
-			var personIds = (from filename in Directory.GetFiles(templates).Take(5)
-			                 select Path.GetFileNameWithoutExtension(filename).Split(new[] { '_' })[0]).ToArray();
+			var personIds = (from filename in Directory.GetFiles(templates)
+				select Path.GetFileNameWithoutExtension(filename).Split(new[] { '_' })[0]).Distinct().ToArray();
 			var fps = (from personId in personIds
-				select (from filename in Directory.GetFiles(templates, personId + "_*.xml").Take(5)
-			                   select new FingerprintTemplate(XElement.Load(filename))).ToArray()).ToArray();
+				select (from filename in Directory.GetFiles(templates, personId + "_*.xml")
+			         select new FingerprintTemplate(XElement.Load(filename))).ToArray()).ToArray();
             var matching = new List<double>();
             var nonmatching = new List<double>();
             foreach (var row in fps)
@@ -55,11 +55,11 @@ namespace SourceAFIS.Tests
 				FNMR = (matching.Count - ApplyThreshold(matching, score)) / (double)matching.Count
 			}).ToList();
 			var eer = thresholds.First(t => t.FNMR >= t.FMR);
-			Console.WriteLine("EER {0:%0.00} @ threshold {1:0.0}", (eer.FMR + eer.FNMR) / 2, eer.Threshold);
+			Console.WriteLine("EER {0:0.00%} @ threshold {1:0.0}", (eer.FMR + eer.FNMR) / 2, eer.Threshold);
 			var fmr1k = thresholds.First(t => t.FMR <= 0.001);
-			Console.WriteLine("FMR1000 {0:%0.00} @ threshold {1:0.0}", fmr1k.FNMR, fmr1k.Threshold);
+			Console.WriteLine("FMR1000 {0:0.00%} @ threshold {1:0.0}", fmr1k.FNMR, fmr1k.Threshold);
 			var fmr10k = thresholds.First(t => t.FMR <= 0.0001);
-			Console.WriteLine("FMR10000 {0:%0.00} @ threshold {1:0.0}", fmr10k.FNMR, fmr10k.Threshold);
+			Console.WriteLine("FMR10000 {0:0.00%} @ threshold {1:0.0}", fmr10k.FNMR, fmr10k.Threshold);
 		}
 
         static int ApplyThreshold(List<double> scores, double threshold)
