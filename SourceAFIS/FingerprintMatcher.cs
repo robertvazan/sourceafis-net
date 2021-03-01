9f4788ee385fc5dedcd0b04f3673358d656a316d
Robert Važan <robert.vazan@tutanota.com>
2021-03-01 22:12:16 +0000

    Updated edge hashing

--- SourceAFIS/FingerprintMatcher.cs
+++ SourceAFIS/FingerprintMatcher.cs
@@ -6,43 +6,41 @@ namespace SourceAFIS
 {
 	public class FingerprintMatcher
 	{
-		void BuildEdgeHash()
+		static Dictionary<int, List<IndexedEdge>> BuildEdgeHash(ImmutableTemplate template)
 		{
-			for (int referenceMinutia = 0; referenceMinutia < Template.Minutiae.Count; ++referenceMinutia)
-				for (int neighborMinutia = 0; neighborMinutia < Template.Minutiae.Count; ++neighborMinutia)
-					if (referenceMinutia != neighborMinutia)
+			var map = new Dictionary<int, List<IndexedEdge>>();
+			for (int reference = 0; reference < template.Minutiae.Length; ++reference)
+				for (int neighbor = 0; neighbor < template.Minutiae.Length; ++neighbor)
+					if (reference != neighbor)
 					{
-						var edge = new IndexedEdge()
-						{
-							Shape = new EdgeShape(Template, referenceMinutia, neighborMinutia),
-							Reference = referenceMinutia,
-							Neighbor = neighborMinutia
-						};
-						foreach (var hash in GetShapeCoverage(edge.Shape))
+						var edge = new IndexedEdge(template.Minutiae, reference, neighbor);
+						foreach (int hash in ShapeCoverage(edge))
 						{
 							List<IndexedEdge> list;
-							if (!EdgeHash.TryGetValue(hash, out list))
-								EdgeHash[hash] = list = new List<IndexedEdge>();
+							if (!map.TryGetValue(hash, out list))
+								map[hash] = list = new List<IndexedEdge>();
 							list.Add(edge);
 						}
 					}
+			return map;
 		}
-
-		static IEnumerable<int> GetShapeCoverage(EdgeShape edge)
+		static List<int> ShapeCoverage(EdgeShape edge)
 		{
-			int minLengthBin = (edge.Length - MaxDistanceError) / MaxDistanceError;
-			int maxLengthBin = (edge.Length + MaxDistanceError) / MaxDistanceError;
-			int angleBins = MathEx.DivRoundUp(256, MaxAngleError);
-			int minReferenceBin = Angle.Difference(edge.ReferenceAngle, MaxAngleError) / MaxAngleError;
-			int maxReferenceBin = Angle.Add(edge.ReferenceAngle, MaxAngleError) / MaxAngleError;
+			int minLengthBin = (edge.Length - Parameters.MaxDistanceError) / Parameters.MaxDistanceError;
+			int maxLengthBin = (edge.Length + Parameters.MaxDistanceError) / Parameters.MaxDistanceError;
+			int angleBins = (int)Math.Ceiling(2 * Math.PI / Parameters.MaxAngleError);
+			int minReferenceBin = (int)(DoubleAngle.Difference(edge.ReferenceAngle, Parameters.MaxAngleError) / Parameters.MaxAngleError);
+			int maxReferenceBin = (int)(DoubleAngle.Add(edge.ReferenceAngle, Parameters.MaxAngleError) / Parameters.MaxAngleError);
 			int endReferenceBin = (maxReferenceBin + 1) % angleBins;
-			int minNeighborBin = Angle.Difference(edge.NeighborAngle, MaxAngleError) / MaxAngleError;
-			int maxNeighborBin = Angle.Add(edge.NeighborAngle, MaxAngleError) / MaxAngleError;
+			int minNeighborBin = (int)(DoubleAngle.Difference(edge.NeighborAngle, Parameters.MaxAngleError) / Parameters.MaxAngleError);
+			int maxNeighborBin = (int)(DoubleAngle.Add(edge.NeighborAngle, Parameters.MaxAngleError) / Parameters.MaxAngleError);
 			int endNeighborBin = (maxNeighborBin + 1) % angleBins;
+			var coverage = new List<int>();
 			for (int lengthBin = minLengthBin; lengthBin <= maxLengthBin; ++lengthBin)
 				for (int referenceBin = minReferenceBin; referenceBin != endReferenceBin; referenceBin = (referenceBin + 1) % angleBins)
 					for (int neighborBin = minNeighborBin; neighborBin != endNeighborBin; neighborBin = (neighborBin + 1) % angleBins)
-						yield return (referenceBin << 24) + (neighborBin << 16) + lengthBin;
+						coverage.Add((referenceBin << 24) + (neighborBin << 16) + lengthBin);
+			return coverage;
 		}
 	}
 }
