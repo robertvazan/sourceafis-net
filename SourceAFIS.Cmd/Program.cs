d3bd9109238268ef33a722b98084316f7d2b2867
Robert Važan <robert.vazan@tutanota.com>
2021-03-04 08:16:14 +0000

    Added CLI project

---
+++ SourceAFIS.Cmd/Program.cs
@@ -0,0 +1,13 @@
+// Part of SourceAFIS for .NET: https://sourceafis.machinezoo.com/net
+﻿using System;
+
+namespace SourceAFIS.Cmd
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
