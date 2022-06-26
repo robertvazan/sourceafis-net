d8f3db4e98e7f105a01050d17440719d36e7952d
Robert Važan <robert.vazan@tutanota.com>
2022-06-26 11:47:00 +0000

    Centralized project configuration: NET 5.0, expanded README, added CONTRIBUTING.md, CI build, README for nuget.org, updated .gitignore, cleanup

--- SourceAFIS/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-// Part of SourceAFIS for .NET: https://sourceafis.machinezoo.com/net
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("SourceAFIS.Tests")]
---
+++ scripts/configure.py
@@ -0,0 +1,31 @@
+# This script generates and updates project configuration files.
+
+# We are assuming that project-config is available in sibling directory.
+# Checkout from https://github.com/robertvazan/project-config
+import pathlib
+project_directory = lambda: pathlib.Path(__file__).parent.parent
+config_directory = lambda: project_directory().parent/'project-config'
+exec((config_directory()/'src'/'net.py').read_text())
+
+root_namespace = lambda: 'SourceAFIS'
+pretty_name = lambda: 'SourceAFIS for .NET'
+subdomain = lambda: 'sourceafis'
+homepage = lambda: website() + 'net'
+inception_year = lambda: 2009
+nuget_description = lambda: 'Fingerprint recognition engine that takes a pair of human fingerprint images and returns their similarity score. Supports efficient 1:N search.'
+nuget_tags = lambda: 'fingerprint; biometrics; authentication; sourceafis'
+test_resources = lambda: ['Resources/*.dat', 'Resources/*.png']
+
+def documentation_links():
+    yield 'SourceAFIS for .NET', homepage()
+    yield 'XML doc comments', readme_dir_url(root_namespace())
+    yield 'SourceAFIS overview', 'https://sourceafis.machinezoo.com/'
+    yield 'Algorithm', 'https://sourceafis.machinezoo.com/algorithm'
+
+def dependencies():
+    use('Dahomey.Cbor:1.16.1')
+def test_dependencies():
+    standard_test_dependencies()
+    use('System.Drawing.Common:5.0.1')
+
+generate()
